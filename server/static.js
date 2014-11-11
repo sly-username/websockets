44675ff7f0df82ed8243a2df89e21f55bc903cce
Ryan Bogle <ryan@eardish.com>
2014-11-11 13:06:47 -0800

    Started groundwork for a static file server for development.
    Using Koa and koa-static node modules
    Koa requires node to be run as `node --harmony`
    
    TODO: make it work

---
+++ server/static.js
@@ -0,0 +1,20 @@
+/**
+ * Created by rj on 11/11/14.
+ */
+
+var serve = require('koa-static'),
+    koa = require('koa'),
+    app = koa();
+
+// $ GET /package.json
+app.use(serve('.'));
+
+// $ GET /hello.txt
+app.use(serve('test/fixtures'));
+
+// or use absolute paths
+app.use(serve(__dirname + '/test/fixtures'));
+
+app.listen(3000);
+
+console.log('listening on port 3000');
