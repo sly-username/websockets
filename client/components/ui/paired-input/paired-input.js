cf4ee32c329f0b052b7503a3c2861837139ea32e
Ryan Bogle <ryan@eardish.com>
2014-11-26 12:13:07 -0800

    Continued Code Review Changes
    Refactored paired-input and added attributeChanged callback.

---
+++ client/components/ui/paired-input/paired-input.js
@@ -0,0 +1,89 @@
+(function( Polymer ) {
+  "use strict";
+
+  var copyableAttributes = ["type", "disabled", "required", "pattern", "placeholder"];
+
+  var copyAttributes = function (elemFrom, elemsTo, attrs) {
+    attrs.forEach(function (attr) {
+      var value;
+      if ( elemFrom.hasAttribute(attr) ) {
+        value = elemFrom.getAttribute(attr);
+        elemsTo.forEach(function(elem){ elem.setAttribute(attr, value); });
+      } else {
+        elemsTo.forEach(function(elem){ elem.removeAttribute(attr); });
+      }
+    });
+  };
+
+  Polymer("paired-input", {
+    _primaryBox: null,
+    _confirmBox: null,
+    _errorDiv: null,
+    _boxes: [],
+    get primaryBox(){
+      return this._primaryBox;
+    },
+    get confirmBox(){
+      return this._confirmBox;
+    },
+    get errorDiv(){
+      return this._errorDiv;
+    },
+    get boxes(){
+      return this._boxes;
+    },
+    ready: function () {
+
+      this._primaryBox = this.shadowRoot.getElementById("primaryBox");
+      this._confirmBox = this.shadowRoot.getElementById("confirmBox");
+      this._errorDiv = this.shadowRoot.getElementById("error");
+      this._boxes = [this.primaryBox, this.confirmBox];
+
+      copyAttributes(this, this.boxes, copyableAttributes);
+
+      if( this.hasAttribute("placeholder") ){
+        this.confirmBox.setAttribute("placeholder", "Confirm " + this.getAttribute("placeholder"));
+      }
+
+      // TODO fix maybe?
+      if ( this.primaryBox.hasAttribute("single-line") ) {
+        this.primaryBox.style.display = this.confirmBox.style.display = "inline";
+      }
+
+
+      if ( (/text|password|email|tel|number|url|search/).test(this.attributes.type.value) ) {
+        return;
+      } else {
+        this.setAttribute("type", "text");
+      }
+    },
+    attached: function () {
+      this.addEventListener("keyup", this.keyConfirm);
+    },
+    keyConfirm: function () {
+      this.errorDiv.innerHTML = this.isValid && this.primaryBox !== "" && this.confirmBox !== "" ? "fields are valid" : "fields are not valid";
+    },
+    get isValid () {
+      return this.primaryBox.validity.valid && (this.primaryBox.value === this.confirmBox.value);
+    },
+    get val () {
+      return [this.primaryBox.value, this.confirmBox.value];
+    },
+    set val (val){
+      this.primaryBox.value = this.confirmBox.value = val;
+      return true;
+    },
+    attributeChanged: function (attrName, oldVal, newVal) {
+      if ( copyableAttributes.some(function(value){ return attrName === value; }) ) {
+        copyAttributes(this, this.boxes, [attrName]);
+      } else if( attrName === "single-line" ){
+        if( newVal == null ){
+          this.primaryBox.style.display = this.confirmBox.style.display = "block";
+        } else {
+          this.primaryBox.style.display = this.confirmBox.style.display = "inline";
+        }
+      }
+    }
+  });
+
+})( Polymer );
