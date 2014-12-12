e68c8c0bbae21372195986441bc5052cdce4d022
Khoa Ngo <khoa@eardish.com>
2014-12-11 17:29:53 -0800

    [#30] created max value and created function that converts values to percentages

--- client/components/ui/progress-bar/progress-bar.js
+++ client/components/ui/progress-bar/progress-bar.js
@@ -6,13 +6,47 @@
       return this._progressValue;
     },
     set progressValue(value){
-      this.attributes["progress-value"].value = value;
+      this.attributes["value"].value = value;
       return (this._progressValue = value);
     },
+    get progressMax(){
+      return this._progressMax;
+    },
+    set progressMax(value){
+      if( this.attributes["max"] ) {
+        this.attributes["max"].value = value;
+        return (this._progressMax = value);
+      } else {
+        return (this._progressMax = 100);
+      }
+    },
+    get pbVal(){
+      return this._pbVal;
+    },
+    set pbVal(value){
+      return (this._pbVal = value);
+    },
     ready: function () {
-      this.progressValue  = this.attributes["progress-value"].value;
-      console.log(this._progressValue);
-      this.shadowRoot.getElementById("bar").style.width = this._progressValue+"%";
+      if( parseInt(this.attributes["value"].value) < 100) {
+        this.progressValue = parseInt(this.attributes["value"].value);
+      } else {
+        alert("IT'S OVER NINE THOUSAND!!!");
+      }
+
+      if ( this.attributes["max"] ) {
+        this.progressMax = parseInt(this.attributes["max"].value) && typeof parseInt(this.attributes["max"].value) == "number" ? parseInt(this.attributes["max"].value) : 100;
+      } else {
+        this.progressMax = 100;
+      }
+
+      if( this.progressValue < this.progressMax) {
+        this.pbVal = Math.round((this.progressValue * 100) / (this.progressMax));
+      } else {
+        alert("Value is larger than Max");
+      }
+
+      this.shadowRoot.getElementById("bar").style.width = this._pbVal+"%";
+
     }
   });
 })( window.Polymer );
