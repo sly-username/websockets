cdacd47ed77dab5a137e8c165e56c90f2c3724a7
Khoa Ngo <khoa@eardish.com>
2014-12-12 09:38:05 -0800

    [#30] jscs stuff

--- client/components/ui/progress-bar/progress-bar.js
+++ client/components/ui/progress-bar/progress-bar.js
@@ -7,46 +7,47 @@
     },
     set progressValue( value ) {
       this.attributes[" value "].value = value;
+
       return ( this._progressValue = value );
     },
     get progressMax() {
       return this._progressMax;
     },
     set progressMax( value ) {
-      if( this.attributes[" max "] ) {
-        this.attributes[ "max" ].value = value;
-        return (this._progressMax = value);
+      if ( this.attributes[" max "] ) {
+        this.attributes[ " max " ].value = value;
+
+        return ( this._progressMax = value );
       } else {
-        return (this._progressMax = 100);
+        return ( this._progressMax = 100 );
       }
     },
-    get pbVal(){
+    get pbVal() {
       return this._pbVal;
     },
-    set pbVal( value ){
+    set pbVal( value ) {
       return ( this._pbVal = value );
     },
-    ready: function () {
-      if( parseInt(this.attributes["value"].value) < 100) {
-        this.progressValue = parseInt(this.attributes["value"].value);
+    ready: function() {
+      if ( parseInt( this.attributes[ " value " ].value ) < 100 ) {
+        this.progressValue = parseInt( this.attributes[ " value " ].value );
       } else {
-        alert("IT'S OVER NINE THOUSAND!!!");
+        alert( "IT'S OVER NINE THOUSAND!!!" );
       }
 
-      if ( this.attributes["max"] ) {
-        this.progressMax = parseInt(this.attributes["max"].value) && typeof parseInt(this.attributes["max"].value) == "number" ? parseInt(this.attributes["max"].value) : 100;
+      if ( this.attributes[ " max " ] ) {
+        this.progressMax = parseInt( this.attributes[ " max " ].value ) && typeof parseInt( this.attributes[ " max " ].value ) == "number" ? parseInt( this.attributes[ " max " ].value ) : 100;
       } else {
         this.progressMax = 100;
       }
 
-      if( this.progressValue < this.progressMax) {
-        this.pbVal = Math.round((this.progressValue * 100) / (this.progressMax));
+      if ( this.progressValue < this.progressMax ) {
+        this.pbVal = Math.round( ( this.progressValue * 100 ) / ( this.progressMax ) );
       } else {
-        alert("Value is larger than Max");
+        alert( " Value is larger than Max " );
       }
 
-      this.shadowRoot.getElementById("bar").style.width = this._pbVal+"%";
-
+      this.shadowRoot.getElementById( " bar " ).style.width = this._pbVal + " % " ;
     }
   });
 })( window.Polymer );
