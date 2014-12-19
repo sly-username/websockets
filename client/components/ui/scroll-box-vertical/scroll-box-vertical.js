87e2c40c90c5f1771d6a5f50eaa50d9a4c6e716d
Khoa Ngo <khoa@eardish.com>
2014-12-18 18:45:27 -0800

    [#49] Got ShowArrow, disabled and scroll up to work... working on scroll down...

--- client/components/ui/scroll-box-vertical/scroll-box-vertical.js
+++ client/components/ui/scroll-box-vertical/scroll-box-vertical.js
@@ -1,7 +1,47 @@
 ( function( Polymer ) {
   "use strict";
-
   Polymer( "scroll-box-vertical", {
+
+    /*** PROPERTIES ***/
+    // Show Arrow
+    get showArrow() {
+      return this._showArrow;
+    },
+    set showArrow( value ) {
+      this.setAttribute( "show-arrows", value );
+
+      return ( this._showArrow = value );
+    },
+    /*** END PROPERTIES ***/
+    /*** FUNCTIONS ***/
+    // Scroll Up
+    scrollUp: function( value ) {
+      var innerBox = this.shadowRoot.getElementsByClassName( "inner-box" )[0];
+      innerBox.scrollTop = parseInt( value, 10 );
+    },
+    // Scroll Down
+    scrollDown: function( value ) {
+      var innerBox = this.shadowRoot.getElementsByClassName( "inner-box" )[0];
+      console.log( innerBox.scrollHeight );
+    },
+    // Listens for disabled
+    disabledChanged: function( oldVal, newVal ) {
+      this.disabled = newVal;
+      this.setAttribute( "disabled", newVal );
+    },
+
+    // Listens for show-arrows, can't listen for disabled due to native disabled
+    attributeChanged: function( attrName, oldVal, newVal ) {
+      switch ( attrName ) {
+        case "show-arrows":
+          this.setAttribute( "show-arrows", newVal );
+          break;
+        default:
+          // do nothing
+          break;
+      }
+    }
+    /*** END FUNCTIONS ***/
 
   });
 })( window.Polymer );
