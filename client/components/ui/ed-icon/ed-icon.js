5eee6c6f7bb67eb9a128d7001d4feef3d97aa914
Khoa Ngo <khoa@eardish.com>
2014-12-29 11:35:52 -0800

    [#59] initialized polymer

---
+++ client/components/ui/ed-icon/ed-icon.js
@@ -0,0 +1,12 @@
+( function( Polymer ) {
+  "use strict";
+
+  Polymer( "ed-icon", {
+    /*** PROPERTIES ***/
+    /*** END PROPERTIES ***/
+    /*** LIFECYCLE ***/
+    /*** END LIFECYCLE ***/
+    /*** FUNCTIONS ***/
+    /*** END FUNCTIONS ***/
+  });
+})( window.Polymer );
