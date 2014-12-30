7312a4f929d08fc7d34f91919d917c8364132fc8
Khoa Ngo <khoa@eardish.com>
2014-12-30 10:01:42 -0800

    [#59] Completed Spec

--- client/components/ed/ed-icon/ed-icon.js
+++ client/components/ed/ed-icon/ed-icon.js
@@ -3,11 +3,21 @@
 
   Polymer( "ed-icon", {
     /*** PROPERTIES ***/
+    _nameList: [ "add189", "alarm52", "basic14", "basic15", "basic16", "basic17", "etc..." ],
     // nameList
+    get nameList() {
+      return this._nameList;
+    },
     /*** END PROPERTIES ***/
-    /*** LIFECYCLE ***/
-    /*** END LIFECYCLE ***/
     /*** FUNCTIONS ***/
+    nameChanged: function( oldVal, newVal ) {
+      this.name = newVal;
+      this.setAttribute( "name", newVal );
+    },
+    rotationChanged: function( oldVal, newVal ) {
+      this.rotation = newVal;
+      this.setAttribute( "rotation", newVal );
+    }
     /*** END FUNCTIONS ***/
   });
 })( window.Polymer );
