5767ee603dc059f29eaf5c5d1f1d59445aff7f82
slysylvia <sylvia@eardish.com>
2014-12-30 18:50:46 -0800

    [#61] Trying to get property setters to work.

--- client/components/ed/ed-scrubber/ed-scrubber.js
+++ client/components/ed/ed-scrubber/ed-scrubber.js
@@ -12,43 +12,21 @@
   };
 
   Polymer( "ed-scrubber", {
-    get min() {
-      return this.min;
-    },
-    set min( value ) {
-      this.setAttribute( "min", value );
-      this.min = value;
-      return value;
-    },
-    get max() {
-      return this.max;
-    },
-    set max( value ) {
-      this.setAttribute( "max", value );
-      this.max = value;
-      return value;
-    },
-    get format() {
-      return this.format;
-    },
-    set format( value ) {
-      this.setAttribute( "format", value );
-      this.format = value;
-      return value;
-    },
-    get type() {
-      return this.type;
-    },
-    set type( value ) {
-      this.setAttribute( "type", value );
-      this.type = value;
-      return value;
-    },
     ready: function() {
       this.scrubberBox = this.shadowRoot.getElementById( "scrubber-box" );
     },
     attached: function() {
       copyAttributes( this, this.scrubberBox, [ "min", "max", "format", "type" ] );
+
+      // this.addEventListener( "mousedown", this.startDragging );
     }
+    // startDragging: function() {
+    //  var rect = this.scrubberBox.getBoudingClientRect();
+    //  var xOffset = window.pageXOffset;
+    //  var cachedLeft = rect.left + xOffset;
+    //  var cachedWidth = rect.width;
+    //  var cachedTop = rect.top + yOffset;
+    //  var cachedHeight = rect.height;
+    // }
   });
 })( window.Polymer );
