b29738c3a276d4ef4971742711413c554caf5782
slysylvia <sylvia@eardish.com>
2015-01-05 14:09:23 -0800

    [#61] Tried using the native html range input.
    Not sure if we can style this, so I'm going to go back to creating the scrubber and its functionality via javascript.

--- client/components/ed/ed-scrubber/ed-scrubber.js
+++ client/components/ed/ed-scrubber/ed-scrubber.js
@@ -11,43 +11,18 @@
     });
   };
 
-
-  var isDown = false;
-  var scrubberDiv;
-  var offset;
-  var mousePosition;
-
   Polymer( "ed-scrubber", {
     ready: function() {
       this.scrubberBox = this.shadowRoot.getElementById( "scrubber-box" );
     },
     attached: function() {
       copyAttributes( this, this.scrubberBox, [ "min", "max", "format", "type" ] );
-
-      this.addEventListener( "mousedown", this.startDragging );
-      this.addEventListener( "mousemove", this.dragging );
-      this.addEventListener( "mouseup", this.stopDragging );
+      this.scrubberBox.addEventListener( "mousedown", this.createScrubber );
+      this.scrubberDiv = document.createElement( "INPUT" );
     },
-    createScrubberDiv: function() {
-      scrubberDiv = document.createElement( "div" );
-
-      // create div
-      this.appendChild( scrubberDiv );
-    },
-    startDragging: function( event ) {
-      isDown = true;
-      offset = scrubberDiv.offsetLeft - event.clientX;
-    },
-    dragging: function( event ) {
-      event.preventDefault();
-
-      if ( isDown ) {
-        mousePosition = event.clientX;
-        scrubberDiv.style.left = ( mousePosition.x + offset[0] ) + "px";
-      }
-    },
-    stopDragging: function() {
-      isDown = false;
+    createScrubber: function() {
+      this.scrubberDiv.setAttribute( "type", "range" );
+      this.scrubberDiv.id = "scrubber-progress";
     }
   });
 })( window.Polymer );
