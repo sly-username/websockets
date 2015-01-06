c8e20255f5bb5797becc4097e6ae1030669bed47
slysylvia <sylvia@eardish.com>
2015-01-05 17:49:39 -0800

    [#61] Scrubbers have been styled and values are printing out.
    CSS still needs to be refactored.

--- client/components/ed/ed-scrubber/ed-scrubber.js
+++ client/components/ed/ed-scrubber/ed-scrubber.js
@@ -12,17 +12,28 @@
   };
 
   Polymer( "ed-scrubber", {
+    get format() {
+      return this._format;
+    },
+    set format( value ) {
+      this.attributes.format.value = value;
+      this._format = value;
+      return value;
+    },
     ready: function() {
-      this.scrubberWrapper = this.shadowRoot.getElementById( "scrubberWrapper" );
-      this.scrubberInput = this.shadowRoot.getElementById( "scrubberInput" );
-      this.valBox = this.shadowRoot.getElementById( "valBox" );
+      this.scrubberWrapper = this.shadowRoot.getElementById( "scrubber-wrapper" );
+      this.scrubberInput = this.shadowRoot.getElementById( "scrubber-input" );
+      this.valBox = this.shadowRoot.getElementById( "val-box" );
     },
     attached: function() {
       copyAttributes( this, this.scrubberInput, [ "min", "max", "format" ]);
       copyAttributes( this, this.scrubberWrapper, [ "type" ]);
     },
     showVal: function() {
-      this.valBox.innerHTML = this.scrubberInput.value;
+      this.valBox.innerHTML = this.format === "percent" ? parseInt( ( ( this.scrubberInput.value - this.scrubberInput.min ) / ( this.scrubberInput.max - this.scrubberInput.min ) * 100 ), 10 ) + "%" : this.scrubberInput.value + " seconds";
     }
   });
 })( window.Polymer );
+
+
+// scrubber value / scrubber max * 100
