44da7158ecb9d8cf0a20d0f9b58eb25d86d92216
slysylvia <sylvia@eardish.com>
2015-01-08 13:51:18 -0800

    [#79] Starting Modal element creation.
    Here we go. html native dialog element is not supported by internet explorer and firefox. So ... we shall build our own.

---
+++ client/components/ed/ed-modal/ed-modal.js
@@ -0,0 +1,29 @@
+( function( Polymer ) {
+  "use strict";
+
+  var copyAttributes = function( elemFrom, elemTo, attrs ) {
+    attrs.forEach( function( attr ) {
+      if ( elemFrom.hasAttribute( attr ) ) {
+        elemTo.setAttribute( attr, elemFrom.getAttribute( attr ) );
+      } else {
+        elemTo.removeAttribute( attr );
+      }
+    });
+  };
+
+  Polymer( "ed-modal", {
+    get clickOff() {
+      return this._format;
+    },
+     set clickOff( value ) {
+       this.attributes.clickOff.value = value;
+       this._format = value;
+       return value;
+     },
+    ready: function() {
+    },
+    attached: function() {
+      copyAttributes( this, this, [ "trigger", "clickOff", "closeButton" ]);
+    }
+  });
+})( window.Polymer );
