fecb6b5c7b6c8c74423a3e019c4cf15457264c7a
Khoa Ngo <khoa@eardish.com>
2015-01-09 17:14:33 -0800

    [#60] polymer-ready functions

--- client/components/ui/img-placeholder/img-placeholder.js
+++ client/components/ui/img-placeholder/img-placeholder.js
@@ -1,12 +1,40 @@
 ( function( Polymer ) {
   "use strict";
 
+  var globalMap = {},
+      updateMap = function() {
+        Array.prototype.forEach.call(
+          document.getElementsByTagName( "img-placeholder" ),
+          function( element ) {
+            element.rePrep();
+            return true;
+          }
+        );
+  }
+
+
   Polymer( "img-placeholder", {
-    /*** FUNCTIONS ***/
-    typeChanged: function( oldVal, newVal ) {
-      this.name = newVal;
-      this.setAttribute( "type", newVal );
+    /*** PROPERTIES ***/
+    get imageMap() {
+      return globalMap;
+    },
+    set imageMap( value ) {
+      globalMap = value;
+      updateMap();
+      return globalMap;
+    },
+    rePrep: function() {
+      if ( !this.src ) {
+        this.fallBack.src = this.src || globalMap[this.image];
+      }
+    },
+    /*** END PROPERTIES ***/
+    /*** LIFECYCLE ***/
+    ready: function() {
+      this.fallBack = this.shadowRoot.getElementsByClassName( "display" )[0];
     }
+    /*** END LIFECYCLE ***/
+    /*** FUNCTIONS ***/
     /*** END FUNCTIONS ***/
   });
 })( window.Polymer );
