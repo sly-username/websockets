901a782959684295202e9058324f988ef69868bc
Khoa Ngo <khoa@eardish.com>
2015-01-12 11:05:48 -0800

    [#84] Initial Commit

---
+++ client/components/ui/file-drop/file-drop.js
@@ -0,0 +1,12 @@
+( function( Polymer ) {
+  "use strict";
+
+  Polymer( "file-drop", {
+    /*** PROPERTIES ***/
+    /*** END PROPERTIES ***/
+    /*** LIFECYCLE ***/
+    /*** END LIFECYCLE ***/
+    /*** FUNCTIONS ***/
+    /*** END FUNCTIONS ***/
+  });
+})( window.Polymer );
