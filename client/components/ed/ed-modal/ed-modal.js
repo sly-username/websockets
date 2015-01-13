0789df8f93a8bca2500bc3e510eabecb98e1be56
slysylvia <sylvia@eardish.com>
2015-01-13 15:59:55 -0800

    [#79] Changing event listener from click to mousedown/touchstart.
    Working on updating on trigger event when trigger ID is changed. Seemed to be working earlier. Not sure what happened since.

--- client/components/ed/ed-modal/ed-modal.js
+++ client/components/ed/ed-modal/ed-modal.js
@@ -12,8 +12,26 @@
   };
 
   Polymer( "ed-modal", {
-    //clickOff: true,
-    //closeButton: true,
+    get clickOff() {
+      return this.hasAttribute( "click-off" );
+    },
+    set clickOff( value ) {
+      if ( value ) {
+        this.setAttribute( "click-off", "" );
+      } else {
+        this.removeAttribute( "click-off" );
+      }
+    },
+    get closeButton() {
+      return this.hasAttribute( "close-button" );
+    },
+    set closeButton( value ) {
+      if ( value ) {
+        // turn on
+      } else {
+        this.removeAttribute( "close-button" );
+      }
+    },
     get trigger() {
       return this._trigger;
     },
@@ -25,19 +43,36 @@
     ready: function() {
       this.modalBox = this.shadowRoot.getElementById( this.attributes.trigger.value );
       this.modalButton = this.shadowRoot.getElementById( "modal-button" );
+      this.openListener = this.open.bind( this );
+      this.closeListener = this.close.bind( this );
     },
     attached: function() {
       copyAttributes( this, this.modalBox, [ "clickOff", "closeButton" ] );
-      document.querySelector( this.attributes.trigger.value ).addEventListener( "click", this.open.bind( this ) );
-      this.modalButton.addEventListener( "click", this.close.bind( this ) );
 
-      if ( !this.hasAttribute( "closeButton" ) ) {
+      this.attachTriggerListener();
+      //[ "mousedown", "touchstart" ].forEach( function( e ) {
+      //  document.querySelector( this.attributes.trigger.value )
+      //    .addEventListener( e, this.open() );
+      //}.bind( this ) );
+
+      [ "mousedown", "touchstart" ].forEach( function( e ) {
+        console.log( this.modalButton );
+        this.modalButton.addEventListener( e, this.closeListener );
+      }.bind( this ) );
+
+      if ( !this.hasAttribute( "close-button" ) ) {
         this.modalButton.style.visibility = "hidden";
       }
-
-      if ( this.hasAttribute( "clickOff" ) ) {
-        this.modalBox.addEventListener( "click", this.close.bind( this ) );
-      }
+    },
+    attachTriggerListener: function() {
+      [ "mousedown", "touchstart" ].forEach( function( e ) {
+        document.querySelector( this.attributes.trigger.value )
+          .addEventListener( e, this.openListener );
+      }.bind( this ) );
+    },
+    removeTriggerListener: function() {
+      document.querySelector( this.attributes.trigger.value )
+        .removeEventListener( "click", this.openListener );
     },
     open: function() {
       this.modalBox.style.visibility = "visible";
@@ -46,20 +81,22 @@
       this.modalBox.style.visibility = "hidden";
     },
     triggerChanged: function( oldVal, newVal ) {
+      this.removeTriggerListener();
       this.trigger = newVal;
       this.setAttribute( "trigger", newVal );
 
       if ( this.hasAttribute( "trigger" ) ) {
         this.trigger = this.attributes.trigger.value;
+        this.attachTriggerListener();
       }
     },
     clickOffChanged: function( oldVal, newVal ) {
       this.clickOff = newVal;
-      this.setAttribute( "clickOff", newVal );
+      this.setAttribute( "click-off", newVal );
     },
     closeButtonChanged: function( oldVal, newVal ) {
       this.closeButton = newVal;
-      this.setAttribute( "closeButton", newVal );
+      this.setAttribute( "close-button", newVal );
     }
   });
 })( window.Polymer );
