0e95690bd864134f718192c2e0971370063bd7cf
Khoa Ngo <khoa@eardish.com>
2015-01-13 13:26:30 -0800

    [#60] Added in promises for case where src exists but is invalid

--- client/components/ui/img-placeholder/img-placeholder.js
+++ client/components/ui/img-placeholder/img-placeholder.js
@@ -41,12 +41,31 @@
       this.fallBack[globalMap[this.image] ? "setAttribute" : "removeAttribute"]( "src", globalMap[this.image] );
     },
     imageChanged: function( oldVal, newVal ) {
+      var promise = this.checkSrc( this.src );
       this.image = newVal;
 
       if ( !this.src ) {
         this.fallBack.src = this.src || globalMap[this.image];
+      } else {
+        promise.then( function( response ) {
+          this.fallBack.src = this.src;
+        }.bind( this ) ).catch( function( error ) {
+          this.fallBack.src = globalMap[this.image];
+        }.bind( this ) );
       }
       this.setAttribute( "image", newVal );
+    },
+    checkSrc: function( src ) {
+      return ( new Promise( function( resolve, reject ) {
+        var tmpImage = new Image();
+        tmpImage.onload = function( event ) {
+          resolve( true );
+        }
+        tmpImage.onerror = function( event ) {
+          resolve( false );
+        }
+        tmpImage.src = src;
+      }) );
     }
     /*** END FUNCTION ***/
   });
