834aa7aa817a685b48bff65bda16b5c8091dc8bf
slysylvia <sylvia@eardish.com>
2015-01-12 18:23:04 -0800

    [#79] Hiding close button working. Clicking off also working.
    Still need to work on the getters and setters for clickOff and closeButton. Not sure why it's not working. May also need to write attribute changed functions. Also need to add mousedown and touchstart event listeners.

--- client/components/ed/ed-modal/ed-modal.js
+++ client/components/ed/ed-modal/ed-modal.js
@@ -1,6 +1,16 @@
 ( function( Polymer ) {
   "use strict";
 
+  var copyAttributes = function( elemFrom, elemTo, attrs ) {
+    attrs.forEach( function( attr ) {
+      if ( elemFrom.hasAttribute( attr ) ) {
+        elemTo.setAttribute( attr, elemFrom.getAttribute( attr ) );
+      } else {
+        elemTo.removeAttribute( attr );
+      }
+    });
+  };
+
   Polymer( "ed-modal", {
     get trigger() {
       return this._trigger;
@@ -10,32 +20,43 @@
       this._trigger = value;
       return value;
     },
-    get clickOff() {
-      return this._clickOff;
-    },
-    set clickOff( value ) {
-
-    },
-    get closeButton() {
-      return this._closeButton;
-    },
-    set closeButton( value ) {
-
-    },
     ready: function() {
       this.modalBox = this.shadowRoot.getElementById( this.attributes.trigger.value );
       this.modalButton = this.shadowRoot.getElementById( "modal-button" );
     },
     attached: function() {
+      copyAttributes( this, this.modalBox, [ "clickOff", "closeButton" ]);
       document.querySelector( this.attributes.trigger.value ).addEventListener( "click", this.open.bind( this ) );
       this.modalButton.addEventListener( "click", this.close.bind( this ) );
+
+      if ( !this.hasAttribute( "closeButton" ) ) {
+        this.modalButton.style.visibility = "hidden";
+      }
+
+      if ( this.hasAttribute( "clickOff" ) ) {
+        this.modalBox.addEventListener( "click", this.close.bind( this ) );
+      }
+    },
+    open: function() {
+      this.modalBox.style.visibility = "visible";
     },
     close: function() {
       this.modalBox.style.visibility = "hidden";
     },
-    open: function() {
-      console.log( this );
-      this.modalBox.style.visibility = "visible";
-    }
+    triggerChanged: function( oldVal, newVal ) {
+      this.trigger = newVal;
+      this.setAttribute( "trigger", newVal );
+
+      if ( this.hasAttribute( "trigger" ) ) {
+        this.trigger = this.attributes.trigger.value;
+      }
+    },
+    //clickOffChanged: function( oldVal, newVal ) {
+    //  this.clickOff = newVal;
+    //  this.setAttribute( "clickOff", "" );
+    //},
+    //closeButtonChanged: function( oldVal, newVal ) {
+    //  this.closeButton = newVal;
+    //}
   });
 })( window.Polymer );
