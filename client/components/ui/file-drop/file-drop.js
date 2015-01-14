f80f7eb5834db5f0595c67a4d2cb788325b1619e
Khoa Ngo <khoa@eardish.com>
2015-01-14 12:37:23 -0800

    [#84] Created Box, and event listeners

--- client/components/ui/file-drop/file-drop.js
+++ client/components/ui/file-drop/file-drop.js
@@ -5,6 +5,24 @@
     /*** PROPERTIES ***/
     /*** END PROPERTIES ***/
     /*** LIFECYCLE ***/
+    ready: function() {
+      this.holder = this.shadowRoot.getElementById( "holder" );
+    },
+    attached: function() {
+      // Listeners for dragEvents
+      this.holder.addEventListener( "dragover", function() {
+        this.holder.className = "hover";
+        return false;
+      }.bind( this ) );
+      this.holder.addEventListener( "drop", function( event ) {
+        event.stopPropagation();
+        event.preventDefault();
+      } );
+      this.holder.addEventListener( "dragleave", function() {
+        this.holder.className = "";
+        return false;
+      }.bind( this ) );
+    }
     /*** END LIFECYCLE ***/
     /*** FUNCTIONS ***/
     /*** END FUNCTIONS ***/
