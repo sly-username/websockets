7110e6100300bc68532340807cfbde7cbd1b682f
Khoa Ngo <khoa@eardish.com>
2015-01-14 14:15:50 -0800

    [#84] Data transfer from drag and drop to hidden input field

--- client/components/ui/file-drop/file-drop.js
+++ client/components/ui/file-drop/file-drop.js
@@ -7,24 +7,35 @@
     /*** LIFECYCLE ***/
     ready: function() {
       this.holder = this.shadowRoot.getElementById( "holder" );
+      this.hiddenInput = this.shadowRoot.getElementById( "hidden-input" );
     },
     attached: function() {
       // Listeners for dragEvents
-      this.holder.addEventListener( "dragover", function() {
-        this.holder.className = "hover";
-        return false;
-      }.bind( this ) );
+      this.holder.addEventListener( "dragover", function( event ) {
+        event.preventDefault();
+      });
+      this.holder.addEventListener( "dragenter", function( event ) {
+        event.target.className = "hover";
+      });
+      this.holder.addEventListener( "dragleave", function( event ) {
+        event.target.className = "";
+      });
       this.holder.addEventListener( "drop", function( event ) {
+        var files = event.dataTransfer.files;
+
+        // prevents the default redirect
         event.stopPropagation();
         event.preventDefault();
-      } );
-      this.holder.addEventListener( "dragleave", function() {
-        this.holder.className = "";
-        return false;
+        event.target.className = "";
+        this.readFiles( files );
       }.bind( this ) );
-    }
+    },
     /*** END LIFECYCLE ***/
     /*** FUNCTIONS ***/
+    readFiles: function( files ) {
+      this.hiddenInput.files = files;
+      console.log( this.hiddenInput.files );
+    }
     /*** END FUNCTIONS ***/
   });
 })( window.Polymer );
