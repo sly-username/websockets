b4613940ba359fc9a4e8c4267652b05f18fc953f
Khoa Ngo <khoa@eardish.com>
2015-01-15 12:18:45 -0800

    [#84] Pausing to try and Extend Native input element.

--- client/components/ui/file-drop/file-drop.js
+++ client/components/ui/file-drop/file-drop.js
@@ -2,16 +2,26 @@
   "use strict";
 
   Polymer( "file-drop", {
+    droppedFiles: {},
     /*** PROPERTIES ***/
     /*** END PROPERTIES ***/
     /*** LIFECYCLE ***/
     ready: function() {
-      this.holder = this.shadowRoot.getElementById( "holder" );
+      //this._acceptable = this.accepts.split( ", " );
+      //console.log( this._acceptable );
+      //this.holder = this.shadowRoot.getElementById( "holder" );
       this.hiddenInput = this.shadowRoot.getElementById( "hidden-input" );
     },
     attached: function() {
       // Listeners for dragEvents
-      this.holder.addEventListener( "dragover", function( event ) {
+      this.addEventListener( "drop", function( event ) {
+        this.hiddenInput.dispatchEvent( event );
+      });
+      this.hiddenInput.addEventListener( "drop", function( event ) {
+        event.stopPropagation();
+      });
+
+      /*this.holder.addEventListener( "dragover", function( event ) {
         event.preventDefault();
       });
       this.holder.addEventListener( "dragenter", function( event ) {
@@ -21,21 +31,33 @@
         event.target.className = "";
       });
       this.holder.addEventListener( "drop", function( event ) {
-        var files = event.dataTransfer.files;
+        var i, j;
+        this.droppedFiles = Array.from( event.dataTransfer.files );
 
+        console.log( 1, this.droppedFiles );
         // prevents the default redirect
         event.stopPropagation();
         event.preventDefault();
         event.target.className = "";
-        this.readFiles( files );
-      }.bind( this ) );
-    },
+
+        // filters for acceptable types
+        for ( i = 0; i < this.droppedFiles.length; i++ ) {
+         //search the string for match instead of looping over accepts again.
+          for ( j = 0; j < this._acceptable.length; j++ ) {
+            if ( this.droppedFiles[i].type === this._acceptable[j] ) {
+              break;
+            } else if ( j === this._acceptable.length - 1 ) {
+              this.droppedFiles.splice( i, 1 );
+              i--;
+            }
+          }
+        }
+        console.log(2, this.droppedFiles );
+
+      }.bind( this ) );*/
+    }
     /*** END LIFECYCLE ***/
     /*** FUNCTIONS ***/
-    readFiles: function( files ) {
-      this.hiddenInput.files = files;
-      console.log( this.hiddenInput.files );
-    }
     /*** END FUNCTIONS ***/
   });
 })( window.Polymer );
