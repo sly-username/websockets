6832a1f34391b97c1cb53ff0882d5fca3a73b77b
Khoa Ngo <khoa@eardish.com>
2015-01-15 18:14:27 -0800

    [#84] Ditching Input

--- client/components/ui/file-drop/file-drop.js
+++ client/components/ui/file-drop/file-drop.js
@@ -2,59 +2,30 @@
   "use strict";
 
   Polymer( "file-drop", {
-    droppedFiles: {},
+    files: [],
     /*** PROPERTIES ***/
     /*** END PROPERTIES ***/
     /*** LIFECYCLE ***/
     ready: function() {
-      //this._acceptable = this.accepts.split( ", " );
-      //console.log( this._acceptable );
-      //this.holder = this.shadowRoot.getElementById( "holder" );
-      this.hiddenInput = this.shadowRoot.getElementById( "hidden-input" );
+      this.hiddenInput = this.shadowRoot.getElementById("hidden-input");
+      this.holder = this.shadowRoot.getElementById( "holder" );
+      // Listeners for dragEvents
     },
     attached: function() {
-      // Listeners for dragEvents
-      this.addEventListener( "drop", function( event ) {
-        this.hiddenInput.dispatchEvent( event );
-      });
-      this.hiddenInput.addEventListener( "drop", function( event ) {
+      this.holder.addEventListener( "dragover", function( event ) {
         event.stopPropagation();
-      });
-
-      /*this.holder.addEventListener( "dragover", function( event ) {
         event.preventDefault();
-      });
-      this.holder.addEventListener( "dragenter", function( event ) {
-        event.target.className = "hover";
-      });
-      this.holder.addEventListener( "dragleave", function( event ) {
-        event.target.className = "";
       });
       this.holder.addEventListener( "drop", function( event ) {
-        var i, j;
-        this.droppedFiles = Array.from( event.dataTransfer.files );
-
-        console.log( 1, this.droppedFiles );
-        // prevents the default redirect
         event.stopPropagation();
         event.preventDefault();
-        event.target.className = "";
-
-        // filters for acceptable types
-        for ( i = 0; i < this.droppedFiles.length; i++ ) {
-         //search the string for match instead of looping over accepts again.
-          for ( j = 0; j < this._acceptable.length; j++ ) {
-            if ( this.droppedFiles[i].type === this._acceptable[j] ) {
-              break;
-            } else if ( j === this._acceptable.length - 1 ) {
-              this.droppedFiles.splice( i, 1 );
-              i--;
-            }
-          }
-        }
-        console.log(2, this.droppedFiles );
-
-      }.bind( this ) );*/
+        this.files = Array.from( event.dataTransfer.files ).filter( function( file ) {
+          var compare = new RegExp( file.type );
+          console.log( compare.test( this.accepts ) );
+          return compare.test( this.accepts );
+        }.bind( this ) );
+        console.log( this.files );
+      }.bind( this ) );
     }
     /*** END LIFECYCLE ***/
     /*** FUNCTIONS ***/
