a2386afe8047e2072be6c27b50983a7bade1af01
Ryan Bogle <ryan@eardish.com>
2015-01-16 19:25:23 -0800

    [#21] Added some more tests for on-off element.

--- client/components/ui/on-off/on-off.tests.js
+++ client/components/ui/on-off/on-off.tests.js
@@ -1,15 +1,7 @@
 /*eslint-env mocha */
-( function() {
+( function( window, document, chai ) {
   "use strict";
   var expect = chai.expect;
-
-  suite( "Array", function() {
-    suite( "#indexOf()", function() {
-      test( "should return -1 when not present", function() {
-        expect( [ 1, 2, 3 ].indexOf( 4 ) ).to.equal( -1 );
-      });
-    });
-  });
 
   suite( "<on-off>", function() {
     var element;
@@ -17,19 +9,46 @@
       element = document.createElement( "on-off" );
     });
 
-    suite( "life cycle", function() {
+    suite( "Life Cycle", function() {
       test( "ready: can create from document.createElement", function() {
         expect( element )
           .to.have.property( "outerHTML" )
           .that.equals( "<on-off></on-off>" );
       });
-    });
-    /*
-    suite( "attributes", function() {
-      test( "on-text", function() {
 
+      test( "attached/detached: can be added/removed to/from the DOM", function() {
+        var div = document.createElement( "div" );
+        div.appendChild( element );
+        expect( div )
+          .to.have.property( "innerHTML" )
+          .that.equals( "<on-off></on-off>" );
+
+        div.removeChild( element );
+        expect( div )
+          .to.have.property( "outerHTML" )
+          .that.equals( "<div></div>" );
       });
     });
-    */
+
+    suite( "Attributes", function() {
+      test( "on-text", function() {
+//        element.onText = "New-Text";
+        expect( element )
+          .to.have.property( "onText" )
+          .that.equals( "New-Text" );
+
+        expect( element )
+          .to.have.property( "outerHTML" )
+          .that.equals( "<on-off on-text=\"New-Text\"></on-off>" );
+      });
+
+      test( "off-text", function() {
+        var div = document.createElement( "div" );
+        div.innerHTML = "<on-off off-text=\"New-Text\"></on-off>";
+        expect( div.children[0] )
+          .to.have.property( "offText" )
+          .that.equals( "New-Text" );
+      });
+    });
   });
-})();
+})( window, document, window.chai );
