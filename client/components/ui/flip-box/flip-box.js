c27a21b8a37d708eb8738d39436a6efd127af5e2
slysylvia <sylvia@eardish.com>
2015-01-20 19:33:21 -0800

    [#36] Begin figuring out how to use CSS to flip the boxes concurrently with the shadow dom.

--- client/components/ui/flip-box/flip-box.js
+++ client/components/ui/flip-box/flip-box.js
@@ -1,1 +1,18 @@
+( function( Polymer ) {
+  "use strict";
 
+  Polymer( "flip-box", {
+
+    /*** LIFECYCLE ***/
+    ready: function() {
+
+    },
+    attached: function() {
+
+    },
+
+    /*** FUNCTIONS ***/
+
+    /*** END FUNCTIONS ***/
+  });
+})( window.Polymer );
