9089bdd527d263579e7aca9255dee9de6cc7c640
slysylvia <sylvia@eardish.com>
2015-01-21 17:19:52 -0800

    [#36] Was able to get front/back content to show via button click.
    Now on to animation ...
    Then we will figure out how to do all of this via hover.

--- client/components/ui/flip-box/flip-box.js
+++ client/components/ui/flip-box/flip-box.js
@@ -2,36 +2,54 @@
   "use strict";
 
   Polymer( "flip-box", {
-              get trigger() {
-                return this._trigger;
-              },
-              set trigger(value) {
-                this.setAttribute("trigger", value);
-                this._trigger = value;
-                return value;
-              },
+    get trigger() {
+      return this._trigger;
+    },
+    set trigger( value ) {
+      this.setAttribute( "trigger", value );
+      this._trigger = value;
+      return value;
+    },
     /*** LIFECYCLE ***/
-    ready:    function() {
-      this.flipboxContainer = this.shadowRoot.getElementsByClassName("flipbox-container")[0];
-      this.flipboxButton = this.shadowRoot.getElementsByClassName("flipbox-button")[0];
+    ready: function() {
+      this.flipBoxContainer = this.shadowRoot.getElementsByClassName( "flipbox-container" )[0];
+      this.backFlipBox = this.shadowRoot.getElementById( "back" );
+      this.frontFlipBox = this.shadowRoot.getElementById( "front" );
+      this.flipBoxButton = this.shadowRoot.getElementsByClassName( "flipbox-button" )[0];
+      this._trigger = this.attributes.trigger.value;
     },
     attached: function() {
-      this._trigger = this.attributes.trigger.value;
       //  this.visibleClass = ["front", "back"];
+      this.btnListener();
+      this.frontFlipBox.classList.add( "front" );
     },
 
     /*** FUNCTIONS ***/
     /* EVENT LISTENERS */
+    btnListener: function() {
+
+
+        this.flipBoxButton.addEventListener( "mousedown", function() {
+          console.log(this.frontFlipBox);
+          this.frontFlipBox.classList.toggle( "front" );
+          console.log(this.frontFlipBox.classList.contains("front"));
+          this.backFlipBox.classList.toggle( "back" );
+          console.log(this.backFlipBox.classList.contains("back"));
+        }.bind( this ) );
+
+    },
+
     // if trigger = btn
     //  then add click listener to this.flipboxButton
     //    when button is clicked,
-    //      then class will change from front/back or back/front
+    //      this.flipbox.classList.toggle("front")
+    //      classList.toggle("back")
     // if trigger = box
     //  then add hover listener to this.flipboxContainer
     //    when user hovers over the flip box,
     //      then class will change from front/back or back/front
 
-    //  needs to know if current class is front or back - for the event handler
+    //  need to know if current class is front or back - for the event handler
     //    if classList contains .front, then remove that and add .back
     //    classList.toggle("front")
     //      will it toggle between front and back?
