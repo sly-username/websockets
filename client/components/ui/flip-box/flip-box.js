3cb4fbe78bbb8321200d956b4e6f8487da10525f
slysylvia <sylvia@eardish.com>
2015-01-22 17:50:18 -0800

    [#36] Flip Box is now functional.
    The animation doesn't seem as sharp as I would like. Now working on achieving the horizontal animation in addition to the vertical one.
    I added test files, but didn't really write any tests yet.

--- client/components/ui/flip-box/flip-box.js
+++ client/components/ui/flip-box/flip-box.js
@@ -10,55 +10,47 @@
       this._trigger = value;
       return value;
     },
+    get animation() {
+      return this._animation;
+    },
+    set animation( value ) {
+      this.setAttribute( "animation", value );
+      this._animation = value;
+      return value;
+    },
     /*** LIFECYCLE ***/
     ready: function() {
-      this.flipBoxContainer = this.shadowRoot.getElementsByClassName( "flipbox-container" )[0];
+      this.flipBoxContainer = this.shadowRoot.getElementsByClassName( "flipbox-container" )[ 0 ];
       this.backFlipBox = this.shadowRoot.getElementById( "back" );
       this.frontFlipBox = this.shadowRoot.getElementById( "front" );
-      this.flipBoxButton = this.shadowRoot.getElementsByClassName( "flipbox-button" )[0];
+      this.flipBoxButtons = Array.from( this.shadowRoot.getElementsByClassName( "flipbox-button" ) );
+      this.boxes = Array.from( this.shadowRoot.getElementsByClassName( "box" ) );
       this._trigger = this.attributes.trigger.value;
     },
     attached: function() {
-      //  this.visibleClass = ["front", "back"];
       this.btnListener();
-      this.frontFlipBox.classList.add( "front" );
+
+      if ( this.attributes.animation.value === "vert" ) {
+        this.boxes.forEach( function( box ) {
+          box.classList.add( "vert" );
+        });
+      }
     },
 
     /*** FUNCTIONS ***/
     /* EVENT LISTENERS */
     btnListener: function() {
-
-
-        this.flipBoxButton.addEventListener( "mousedown", function() {
-          console.log(this.frontFlipBox);
-          this.frontFlipBox.classList.toggle( "front" );
-          console.log(this.frontFlipBox.classList.contains("front"));
-          this.backFlipBox.classList.toggle( "back" );
-          console.log(this.backFlipBox.classList.contains("back"));
+      this.flipBoxButtons.forEach( function( button ) {
+        button.addEventListener( "mousedown", function() {
+          this.flipBoxContainer.classList.toggle( "flip" );
         }.bind( this ) );
-
+      }.bind( this ) );
     },
 
-    // if trigger = btn
-    //  then add click listener to this.flipboxButton
-    //    when button is clicked,
-    //      this.flipbox.classList.toggle("front")
-    //      classList.toggle("back")
     // if trigger = box
     //  then add hover listener to this.flipboxContainer
     //    when user hovers over the flip box,
     //      then class will change from front/back or back/front
-
-    //  need to know if current class is front or back - for the event handler
-    //    if classList contains .front, then remove that and add .back
-    //    classList.toggle("front")
-    //      will it toggle between front and back?
-
-    //  this.flipbox.classList.toggle("front");
-    //    can you toggle multiple classes?
-    //    it will start out with front. so the toggle will remove front
-    //    and the back toggle will add back.
-    //    hopefully.
 
     /* ATTRIBUTE CHANGE */
     attributeChanged: function( attrName, oldVal, newVal ) {
