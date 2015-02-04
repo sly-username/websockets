0c6010a4cde32445b844015eb2cb79ee1529fe80
Ryan Bogle <ryan@eardish.com>
2015-02-03 22:51:08 -0800

    [#109] Breakpoints work in a minimal fashion
    We will need to add cases for when the device has a high dpi, to knock
    the down breakpoint type one step.
    
    Will look into new ways to organize the mixins so they don't have to be
    a bunch of nested calls which ends up looking like callback hell.

---
+++ client/components/poc/poc-bp/poc-bp.js
@@ -0,0 +1,13 @@
+( function( Polymer ) {
+  "use strict";
+
+  Polymer( "poc-bp", {
+    /*** LIFECYCLE ***/
+    ready: function() {},
+    attached: function() {},
+    detached: function() {},
+    attributeChanged: function( attrName, oldVal, newVal ) {}
+    /*** PROPERTIES ***/
+    /*** METHODS ***/
+  });
+})( window.Polymer );
