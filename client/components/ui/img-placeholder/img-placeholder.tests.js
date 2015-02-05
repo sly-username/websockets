7c72e0e744ba2cbaeb569588419a82ae91c8f905
Khoa Ngo <khoa@eardish.com>
2015-02-04 17:52:00 -0800

    [#100] Pull Ready

--- client/components/ui/img-placeholder/img-placeholder.tests.js
+++ client/components/ui/img-placeholder/img-placeholder.tests.js
@@ -13,7 +13,7 @@
             ];
 
         imageNames.forEach( function( imageName ) {
-          map[imageName] = urlPrefix + imageName + urlPostfix;
+          map[ imageName ] = urlPrefix + imageName + urlPostfix;
         });
         return map;
       })(),
@@ -70,7 +70,7 @@
       suite( "Image", function() {
         test( "can be set via \"setAttribute\"", function() {
           var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Attribute";
+              setTo = "placecage";
 
           imgPlaceholder.setAttribute( "image", setTo );
 
@@ -87,7 +87,7 @@
 
         test( "can be set via property \"image\"", function() {
           var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Property";
+              setTo = "placecage";
 
           imgPlaceholder.image = setTo;
 
@@ -98,7 +98,7 @@
 
         test( "setting via \"setAttribute\" reflects to property \"image\"", function() {
           var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Attribute";
+              setTo = "placecage";
 
           imgPlaceholder.setAttribute( "image", setTo );
 
@@ -110,7 +110,7 @@
 
         test( "setting via property \"image\" reflects to attribute \"image\"", function() {
           var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Property";
+              setTo = "placecage";
 
           imgPlaceholder.image = setTo;
 
@@ -121,54 +121,202 @@
             .and.equal( imgPlaceholder.image );
         });
 
-        test( "removing attribute \"image\" sets property back to default value", function() {
-          var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Attribute";
+        test( "setting \"image\" via property uses the imageMap url", function() {
+          var imgPlaceholder = document.createElement( "img-placeholder" );
 
-          imgPlaceholder.setAttribute( "image", setTo );
-          expect( imgPlaceholder.hasAttribute( "image" ) ).to.equal( true );
+          imgPlaceholder.image = "placecage";
 
-          imgPlaceholder.removeAttribute( "image" );
-          expect( imgPlaceholder.hasAttribute( "image" ) ).to.equal( false );
-
-          expect( imgPlaceholder )
-            .to.have.property( "image" )
+          expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+            .to.have.property( "src" )
             .that.is.a( "string" )
-            .and.equals( "On" );
+            .and.equals( imageMap.placecage );
         });
 
-        test( "setting \"image\" to null resets property to default value", function() {
-          var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Property";
+        test( "setting \"image\" via attribute uses the imageMap url", function() {
+          var imgPlaceholder = document.createElement( "img-placeholder" );
 
-          imgPlaceholder.image = setTo;
-          expect( imgPlaceholder )
-            .to.have.property( "image" )
-            .that.is.a( "string" )
-            .and.equals( setTo );
+          imgPlaceholder.setAttribute( "image", "placecage" );
 
-          imgPlaceholder.image = null;
-          expect( imgPlaceholder )
-            .to.have.property( "image" )
+          expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+            .to.have.property( "src" )
             .that.is.a( "string" )
-            .and.equals( setTo );
+            .and.equals( imageMap.placecage );
         });
 
-        test( "setting \"image\" to undefined resets property to default value", function() {
-          var imgPlaceholder = document.createElement( "img-placeholder" ),
-              setTo = "Set via Property";
+        suite( "Src", function() {
+          test( "can be set via \"setAttribute\"", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                setTo = "http://www.placecage.com/200/300";
 
-          imgPlaceholder.image = setTo;
-          expect( imgPlaceholder )
-            .to.have.property( "image" )
-            .that.is.a( "string" )
-            .and.equals( setTo );
+            imgPlaceholder.setAttribute( "src", setTo );
 
-          imgPlaceholder.image = undefined;
-          expect( imgPlaceholder )
-            .to.have.property( "image" )
-            .that.is.a( "string" )
-            .and.equals( setTo );
+            expect( imgPlaceholder.hasAttribute( "src" ) ).to.equal( true );
+
+            expect( imgPlaceholder.getAttribute( "src" ) )
+              .to.be.a( "string" )
+              .that.equals( setTo );
+
+            expect( imgPlaceholder )
+              .to.have.property( "outerHTML" )
+              .that.equals( "<img-placeholder src=\"" + setTo + "\"></img-placeholder>" );
+          });
+
+          test( "can be set via property \"src\"", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                setTo = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.src = setTo;
+
+            expect( imgPlaceholder )
+              .to.have.property( "src" )
+              .that.equals( setTo );
+          });
+
+          test( "setting via \"setAttribute\" reflects to property \"src\"", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                setTo = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.setAttribute( "src", setTo );
+
+            expect( imgPlaceholder )
+              .to.have.property( "src" )
+              .that.equals( setTo )
+              .and.equals( imgPlaceholder.getAttribute( "src" ) );
+          });
+
+          test( "setting via property \"src\" reflects to attribute \"src\"", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                setTo = "Set via Property";
+
+            imgPlaceholder.src = setTo;
+
+            expect( imgPlaceholder.hasAttribute( "src" ) ).to.equal( true );
+            expect( imgPlaceholder.getAttribute( "src" ) )
+              .to.be.a( "string" )
+              .that.equals( setTo )
+              .and.equal( imgPlaceholder.src );
+          });
+
+          test( "using src will override image via attribute", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                srcImg = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.setAttribute( "image", "fillmurray" );
+            imgPlaceholder.setAttribute( "src", srcImg );
+
+            expect( imgPlaceholder.hasAttribute( "src" ) ).to.equal( true );
+            expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( srcImg );
+          });
+
+          test( "removing attribute \"src\" uses image fallback", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" );
+
+            imgPlaceholder.setAttribute( "image", "fillmurray" );
+            imgPlaceholder.setAttribute( "src", "http://www.placecage.com/200/300" );
+
+            expect( imgPlaceholder.hasAttribute( "image" ) ).to.equal( true );
+            expect( imgPlaceholder.hasAttribute( "src" ) ).to.equal( true );
+
+            imgPlaceholder.removeAttribute( "src" );
+
+            expect( imgPlaceholder.hasAttribute( "src" ) ).to.equal( false );
+            expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( imageMap.fillmurray );
+          });
+
+          test( "setting \"src\" to null uses image fallback", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                srcImg = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.image = "fillmurray";
+            imgPlaceholder.src = srcImg;
+            expect( imgPlaceholder )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( srcImg );
+
+            imgPlaceholder.src = null;
+            expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( imageMap.fillmurray );
+          });
+
+          test( "setting \"src\" to undefined uses image fallback", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                srcImg = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.image = "fillmurray";
+            imgPlaceholder.src = srcImg;
+            expect( imgPlaceholder )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( srcImg );
+
+            imgPlaceholder.src = undefined;
+            expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( imageMap.fillmurray );
+          });
+
+          test( "if src isnt valid use fallback", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                innerImg = imgPlaceholder.shadowRoot.querySelector( "img" ),
+                srcImg = "http://www.placecage.com/200/300";
+
+            imgPlaceholder.image = "fillmurray";
+            imgPlaceholder.src = srcImg;
+
+            expect( innerImg )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( srcImg );
+
+            imgPlaceholder.src = "WaitThisIsn'tARealImage";
+
+            expect( innerImg )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( imageMap.fillmurray );
+          });
+        });
+
+        suite( "ImageMap", function() {
+          test( "can be set via \"property\"", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                newMap = {
+                  stevensegallery: "http://stevensegallery.com/200/300",
+                  fillmurray: "http://fillmurray.com/200/300"
+                };
+
+            imgPlaceholder.imageMap = newMap;
+            expect( imgPlaceholder )
+              .to.have.property( "imageMap" )
+              .that.is.a( "object" )
+              .and.equals( newMap );
+          });
+
+          test( "can set new image with new imageMap", function() {
+            var imgPlaceholder = document.createElement( "img-placeholder" ),
+                newMap = {
+                  stevensegallery: "http://stevensegallery.com/200/300",
+                  fillmurray: "http://fillmurray.com/200/300"
+                };
+
+            imgPlaceholder.imageMap = newMap;
+            imgPlaceholder.image = "stevensegallery";
+
+            expect( imgPlaceholder.shadowRoot.querySelector( "img" ) )
+              .to.have.property( "src" )
+              .that.is.a( "string" )
+              .and.equals( newMap.stevensegallery );
+          });
         });
       });
     });
