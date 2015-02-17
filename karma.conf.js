e77b285e7f75674384d0036e341819c03bc88b5e
Ryan Bogle <ryan@eardish.com>
2015-02-17 00:55:19 -0800

    [#46] More travis tweaking

--- karma.conf.js
+++ karma.conf.js
@@ -95,7 +95,7 @@ module.exports = function( config ) {
     //    config.LOG_WARN
     //    config.LOG_INFO
     //    config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
