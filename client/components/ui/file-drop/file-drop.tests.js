993d5bc5b06b57a48afebf8737f6a1d153a8f440
Khoa Ngo <khoa@eardish.com>
2015-02-23 15:18:23 -0800

    [#190] Pull Ready

--- client/components/ui/file-drop/file-drop.tests.js
+++ client/components/ui/file-drop/file-drop.tests.js
@@ -38,7 +38,8 @@
         createdSpy.restore();
       });
 
-      test( "attached: can be added to another DOM Element", function() {
+      // Omitted due to polymer not generating an attached function
+      test.skip( "attached: can be added to another DOM Element", function() {
         var newElement = document.createElement( "file-drop" ),
           attachedSpy = sinon.spy( newElement, "attached" );
 
@@ -54,7 +55,8 @@
         attachedSpy.restore();
       });
 
-      test( "detached: can be removed from another DOM element", function() {
+      // Omitted due to polymer not generating an detached function
+      test.skip( "detached: can be removed from another DOM element", function() {
         var newElement = document.createElement( "file-drop" ),
           detachedSpy = sinon.spy( newElement, "detached" );
 
@@ -73,14 +75,187 @@
     });
 
     suite( "Attributes", function() {
-      suite( "data-text", function() {
-        test( "reflection", function() {});
-      });
-    });
+      suite( "Fill", function() {
+        test( "can be set via \"setAttribute\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
 
-    suite( "Events", function() {
-      suite( "Event Name", function() {
-        test( "customEvent", function( done ) {});
+          fileDrop.setAttribute( "fill", "parent" );
+
+          expect( fileDrop.hasAttribute( "fill" ) ).to.equal( true );
+
+          expect( fileDrop.getAttribute( "fill" ) )
+            .to.be.a( "string" )
+            .that.equals( "parent" );
+
+          expect( fileDrop )
+            .to.have.property( "outerHTML" )
+            .that.equals( "<file-drop fill=\"parent\"></file-drop>" );
+        });
+
+        test( "can be set via property \"fill\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.fill = "parent";
+
+          expect( fileDrop )
+            .to.have.property( "fill" )
+            .that.equals( "parent" );
+        });
+
+        test( "setting via \"setAttribute\" reflects to property \"fill\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "fill", "parent" );
+
+          expect( fileDrop )
+            .to.have.property( "fill" )
+            .that.equals( "parent" )
+            .and.equals( fileDrop.getAttribute( "fill" ) );
+        });
+
+        test( "setting via property \"fill\" reflects to attribute \"fill\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.fill = "parent";
+
+          expect( fileDrop.hasAttribute( "fill" ) ).to.equal( true );
+          expect( fileDrop.getAttribute( "fill" ) )
+            .to.be.a( "string" )
+            .that.equals( "parent" )
+            .and.equal( fileDrop.fill );
+        });
+      });
+
+      suite( "Accepts", function() {
+        test( "can be set via \"setAttribute\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "accepts", "image/png" );
+
+          expect( fileDrop.hasAttribute( "accepts" ) ).to.equal( true );
+
+          expect( fileDrop.getAttribute( "accepts" ) )
+            .to.be.a( "string" )
+            .that.equals( "image/png" );
+
+          expect( fileDrop )
+            .to.have.property( "outerHTML" )
+            .that.equals( "<file-drop accepts=\"image/png\"></file-drop>" );
+        });
+
+        test( "can be set via property \"accepts\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.accepts = "image/png";
+
+          expect( fileDrop )
+            .to.have.property( "accepts" )
+            .that.equals( "image/png" );
+        });
+
+        test( "setting via \"setAttribute\" reflects to property \"accepts\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "accepts", "image/png" );
+
+          expect( fileDrop )
+            .to.have.property( "accepts" )
+            .that.equals( "image/png" )
+            .and.equals( fileDrop.getAttribute( "accepts" ) );
+        });
+
+        test( "setting via property \"accepts\" reflects to attribute \"accepts\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.accepts = "image/png";
+
+          expect( fileDrop.hasAttribute( "accepts" ) ).to.equal( true );
+          expect( fileDrop.getAttribute( "accepts" ) )
+            .to.be.a( "string" )
+            .that.equals( "image/png" )
+            .and.equal( fileDrop.accepts );
+        });
+      });
+
+      suite( "multiple", function() {
+        test( "can be set via \"setAttribute\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "multiple", "" );
+          expect( fileDrop.hasAttribute( "multiple" ) ).to.equal( true );
+          expect( fileDrop.getAttribute( "multiple" ) )
+            .to.be.a( "string" )
+            .and.equal( "" );
+        });
+
+        test( "can be set via property \"multiple\"", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.multiple = true;
+          expect( fileDrop )
+            .to.have.property( "multiple" )
+            .that.is.a( "boolean" )
+            .and.equals( true );
+        });
+
+        test( "setting via attribute reflects to property", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "multiple", "" );
+          expect( fileDrop )
+            .to.have.property( "multiple" )
+            .that.is.a( "boolean" )
+            .and.equals( true );
+        });
+
+        test( "setting via property reflects to attribute", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.multiple = true;
+          expect( fileDrop.hasAttribute( "multiple" ) )
+            .to.be.a( "boolean" )
+            .and.to.equal( true );
+        });
+
+        // remove attribute sets to false
+        test( "removing attribute reflects to property", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.setAttribute( "multiple", "" );
+          expect( fileDrop.hasAttribute( "multiple" ) )
+            .to.be.a( "boolean" )
+            .and.to.equal( true );
+
+          fileDrop.removeAttribute( "multiple" );
+          expect( fileDrop )
+            .to.have.property( "multiple" )
+            .that.is.a( "boolean" )
+            .and.equals( false );
+        });
+
+        test( "setting property to false removes attribute", function() {
+          var fileDrop = document.createElement( "file-drop" );
+
+          fileDrop.multiple = true;
+          expect( fileDrop )
+            .to.have.property( "multiple" )
+            .that.is.a( "boolean" )
+            .and.equals( true );
+
+          expect( fileDrop.hasAttribute( "multiple" ) )
+            .to.be.a( "boolean" )
+            .that.equals( true );
+
+          fileDrop.multiple = false;
+          expect( fileDrop )
+            .to.have.property( "multiple" )
+            .that.is.a( "boolean" )
+            .and.equals( false );
+
+          expect( fileDrop.hasAttribute( "multiple" ) )
+            .to.be.a( "boolean" )
+            .and.to.equal( false );
+        });
       });
     });
   });
