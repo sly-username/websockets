ef3f5a43d6c969d6d4a29689f88d9fd6f2b0cb83
Khoa Ngo <khoa@eardish.com>
2015-02-24 13:12:15 -0800

    [#151] Initial Commit

--- client/components/ui/scroll-box-vertical/scroll-box-vertical.tests.js
+++ client/components/ui/scroll-box-vertical/scroll-box-vertical.tests.js
@@ -1,54 +1,74 @@
 /*eslint-env mocha */
-/*eslint-env mocha */
-( function( window, document, chai ) {
+/*jscs:disable maximumLineLength*/
+( function( window, document, polymer, sinon, chai ) {
   "use strict";
   var expect = chai.expect,
-    // get wrapper from document or for karma, create a new div and append it to the DOM
-    testingWrapper = document.getElementById( "scroll-box-vertical-test-wrapper" ) ||
-      ( function() {
-        var wrapper = document.createElement( "div" );
-        document.body.appendChild( wrapper );
-        return wrapper;
-      })(),
-    // original state to test against
-    originalWrapperOuterHTML = testingWrapper.outerHTML,
-    // re-sets wrapper to blank
-    resetWrapper = function() {
-      testingWrapper.innerHTML = "";
-    };
+      // get wrapper from document or for karma, create a new div and append it to the DOM
+      testingWrapper = document.getElementById( "scroll-box-horizontal-test-wrapper" ) ||
+        ( function() {
+          var wrapper = document.createElement( "div" );
+          document.body.appendChild( wrapper );
+          return wrapper;
+        })(),
+      // original state to test against
+      originalWrapperOuterHTML = testingWrapper.outerHTML,
+      // re-sets wrapper to blank
+      resetWrapper = function() {
+        testingWrapper.innerHTML = "";
+      };
 
-  suite( "<scroll-box-vertical>", function() {
+  suite( "<scroll-box-horizontal>", function() {
     suite( "Life Cycle", function() {
+      teardown( function() {
+        resetWrapper();
+      });
+
       test( "ready: can create from document.createElement", function() {
-        expect( document.createElement( "scroll-box-vertical" ) )
+        var createdSpy = sinon.spy(
+          polymer.getRegisteredPrototype( "scroll-box-horizontal" ),
+          "ready"
+        );
+
+        expect( document.createElement( "scroll-box-horizontal" ) )
           .to.have.property( "outerHTML" )
           .that.is.a( "string" )
-          .and.equals( "<scroll-box-vertical></scroll-box-vertical>" );
+          .and.equals( "<scroll-box-horizontal></scroll-box-horizontal>" );
+
+        expect( createdSpy ).to.have.callCount( 1 );
+        createdSpy.restore();
       });
 
       test( "attached: can be added to another DOM Element", function() {
-        testingWrapper.appendChild( document.createElement( "scroll-box-vertical" ) );
+        var newElement = document.createElement( "scroll-box-horizontal" ),
+            attachedSpy = sinon.spy( newElement, "attached" );
+
+        testingWrapper.appendChild( newElement );
+
+        expect( attachedSpy ).to.have.callCount( 1 );
 
         expect( testingWrapper )
           .to.have.property( "innerHTML" )
           .that.is.a( "string" )
-          .and.equals( "<scroll-box-vertical></scroll-box-vertical>" );
+          .and.equals( "<scroll-box-horizontal></scroll-box-horizontal>" );
 
-        resetWrapper();
+        attachedSpy.restore();
       });
 
       test( "detached: can be removed from another DOM element", function() {
-        var sbVert = document.createElement( "scroll-box-vertical" );
+        var newElement = document.createElement( "scroll-box-horizontal" ),
+            detachedSpy = sinon.spy( newElement, "detached" );
 
-        testingWrapper.appendChild( sbVert );
-        testingWrapper.removeChild( sbVert );
+        testingWrapper.appendChild( newElement );
+        testingWrapper.removeChild( newElement );
+
+        expect( detachedSpy ).to.have.callCount( 1 );
 
         expect( testingWrapper )
           .to.have.property( "outerHTML" )
           .that.is.a( "string" )
           .and.equals( originalWrapperOuterHTML );
 
-        resetWrapper();
+        detachedSpy.restore();
       });
     });
 
@@ -262,4 +282,4 @@
       });
     });
   });
-})( window, document, window.chai );
+})( window, document, window.Polymer, window.sinon, window.chai );
