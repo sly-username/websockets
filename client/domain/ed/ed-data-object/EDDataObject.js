5ee20e1b6fd6706d0ddfa4a1abe207537b0b5151
Khoa Ngo <khoa@eardish.com>
2015-03-16 09:33:06 -0700

    [#274] Imported over defined-properties and scaffolded out structure

--- client/domain/ed/ed-data-object/EDDataObject.js
+++ client/domain/ed/ed-data-object/EDDataObject.js
@@ -1,4 +1,20 @@
+import define from "domain/ed/define-properties";
+
+var toStringTag = Symbol( "toStringTag" ); // jshint ignore:line
+
 export default class EDDataObject {
-  constructor( argument ) {
+  constructor( args, id, type ) {
+    this.args = args;
+    args.id = id;
+    args.type = type;
+  }
+}
+
+export class EDDataObject extends EventEmitter {
+  constructor( args ) {
+    define.readOnly( this, [ "id", "name" ], args );
+    define.readOnly( this, [ "type", "object" ], args );
+
+    this[ toStringTag ] = args;
   }
 }
