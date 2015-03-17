6c28f0ec83c9f3e7b60b3020586beec2a5b8e82f
Khoa Ngo <khoa@eardish.com>
2015-03-17 09:58:40 -0700

    [#276] Deleted incorrect code

--- client/domain/ed/objects/EDUser.js
+++ client/domain/ed/objects/EDUser.js
@@ -2,23 +2,6 @@
 import define from "domain/ed/define-properties";
 import EDDataObject from "domain/ed/objects/EDDataObject";
 
-// DELETE ME
-export default class EDUser {
-  constructor( args, username, email, name, firstName, middleName, lastName, prefix, postfix, birthday ) {
-    this.args = args
-    args.username = username;
-    args.email = email;
-    args.name = name;
-    args.name.first = firstName;
-    args.name.middle = middleName;
-    args.name.last = lastName;
-    args.name.prefix = prefix;
-    args.name.postfix = postfix;
-    args.birthday = birthday;
-  }
-}
-// END DELETE ME
-
 export default class EDUser extends EDDataObject {
   static get TYPE() {
     return "user";
@@ -26,6 +9,6 @@ export default class EDUser extends EDDataObject {
 
   constructor( args ) {
     super();
-    define.readOnly( this, [ "email", "email" ], args );
+    define.readOnly( this, [ "username", "email" ], args );
   }
 }
