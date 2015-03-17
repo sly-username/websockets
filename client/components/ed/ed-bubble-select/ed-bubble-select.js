e6c78a27988692d118a885cfffafcd559a3d51c6
Khoa Ngo <khoa@eardish.com>
2015-03-17 15:44:02 -0700

    [#283] creating new component ed-bubble-select and cleaned up jscs errors

---
+++ client/components/ed/ed-bubble-select/ed-bubble-select.js
@@ -0,0 +1,64 @@
+( function( polymer ) {
+  "use strict";
+
+  var copyAttributes = function( elemFrom, elemTo, attrs ) {
+    attrs.forEach( function( attr ) {
+      if ( elemFrom.hasAttribute( attr ) ) {
+        elemTo.setAttribute( attr, elemFrom.getAttribute( attr ) );
+      } else {
+        elemTo.removeAttribute( attr );
+      }
+    });
+  };
+
+  polymer( "ed-bubble-select", {
+    required: false,
+    disabled: false,
+    checked: false,
+    publish: {
+      value: {
+        reflect: true
+      },
+      name: {
+        reflect: true
+      }
+    },
+    ready: function() {
+      this.checkboxField = this.shadowRoot.getElementById( "checkbox-field" );
+
+      this.addEventListener( "click", function() {
+        if ( this.hasAttribute( "disabled" ) ) {
+          return false;
+        }
+
+        this.checked = !this.checked;
+        this[ this.checked ? "setAttribute" : "removeAttribute" ]( "checked", "" );
+        this.children[ 0 ].style.transform = "scale(1)";
+      });
+    },
+    attached: function() {
+      copyAttributes( this, this.checkboxField, [ "checked", "disabled", "required" ]);
+    },
+    requiredChanged: function( oldValue, newValue ) {
+      if ( newValue ) {
+        this.setAttribute( "required", "" );
+      } else {
+        this.removeAttribute( "required" );
+      }
+    },
+    disabledChanged: function( oldValue, newValue ) {
+      if ( newValue ) {
+        this.setAttribute( "disabled", "" );
+      } else {
+        this.removeAttribute( "disabled" );
+      }
+    },
+    checkedChanged: function( oldValue, newValue ) {
+      if ( newValue ) {
+        this.setAttribute( "checked", "" );
+      } else {
+        this.removeAttribute( "checked" );
+      }
+    }
+  });
+})( Polymer );
