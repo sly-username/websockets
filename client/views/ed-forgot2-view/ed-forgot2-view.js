1299437a3fcc97bd7df424f840a977fe40afacdf
Khoa Ngo <khoa@eardish.com>
2015-03-20 16:06:05 -0700

    [#282] Created second forgot password view

---
+++ client/views/ed-forgot2-view/ed-forgot2-view.js
@@ -0,0 +1,26 @@
+( function( polymer ) {
+  "use strict";
+
+  polymer( "ed-forgot2-view", {
+    /* LIFECYCLE */
+    ready: function() {
+      this.pairedInput = this.shadowRoot.querySelector( "ed-paired-input" );
+      this.formContainer = this.shadowRoot.getElementById( "forgot-form" );
+      this.submitButton = this.shadowRoot.getElementById( "forgot-submit" );
+    },
+    attached: function() {
+      this.formContainer.addEventListener( "keyup", this.submitCheck.bind( this ) );
+    },
+    submitCheck: function() {
+      if ( this.pairedInput.isValid ) {
+        this.submitButton.removeAttribute( "disabled" );
+      } else {
+        this.submitButton.setAttribute( "disabled", "" );
+      }
+    },
+    detached: function() {},
+    attributeChanged: function( attrName, oldValue, newValue ) {}
+    /* PROPERTIES */
+    /* METHODS */
+  });
+})( window.Polymer );
