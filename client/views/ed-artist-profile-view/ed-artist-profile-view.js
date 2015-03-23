11b4646de1c2edb7c32de716f850a8739a1878da
Khoa Ngo <khoa@eardish.com>
2015-03-23 13:57:48 -0700

    [#310] initial commit, creating artist page

---
+++ client/views/ed-artist-profile-view/ed-artist-profile-view.js
@@ -0,0 +1,13 @@
+( function( polymer ) {
+  "use strict";
+
+  polymer( "ed-artist-profile-view", {
+    /* LIFECYCLE */
+    ready: function() {},
+    attached: function() {},
+    detached: function() {},
+    attributeChanged: function( attrName, oldValue, newValue ) {}
+    /* PROPERTIES */
+    /* METHODS */
+  });
+})( window.Polymer );
