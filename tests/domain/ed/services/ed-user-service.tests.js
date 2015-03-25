cddcc201d35f38394d8dd92384d41853c042c5bc
sly <sylvia@eardish.com>
2015-03-24 17:25:09 -0700

    [#277] Outlined tests that appear to be needed.
    There are no actual tests, however. Just headings.

--- tests/domain/ed/services/ed-user-service.tests.js
+++ tests/domain/ed/services/ed-user-service.tests.js
@@ -1,7 +1,7 @@
 ( function( win, doc, System, sinon, expect ) {
   "use strict";
 
-  suite( "CreateEvent", function() {
+  suite( "userService", function() {
     var userService;
 
     suiteSetup( function( done ) {
@@ -18,11 +18,115 @@
 
 // Tests begin
     suite( "Properties", function() {
-      test( "currentUser", function() {
+      suite( "currentUser", function() {
+        test( "currentUser default value is null", function() {
 
+        });
+
+        test( "currentUser returns currently logged in EDUser object", function() {
+
+        });
+
+        test( "cannot be set via \"currentUser\" property", function() {
+
+
+          userService.currentUser = "value";
+          expect( userService )
+            .to.have.property( "currentUser" )
+            .that.equals( null );
+        });
       });
 
-      test( "")
+
+      suite( "isOpenSession", function() {
+        test( "isOpenSession default value is false", function() {
+
+        });
+
+        test( "isOpenSession returns true if there is a user logged in", function() {
+
+        });
+
+        test( "cannot be set via \"isOpenSession\" property", function() {
+
+
+          userService.isOpenSession = true;
+          expect( userService )
+            .to.have.property( "isOpenSession" )
+            .that.is.a( "boolean" )
+            .that.equals( false );
+        });
+      });
+
+      suite( "hasOnboarded", function() {
+        test( "hasOnboarded default value is false", function() {
+
+        });
+
+        test( "hasOnboarded returns true if user has already registered", function() {
+
+        });
+
+        test( "cannot be set via \"hasOnboarded\" property", function() {
+
+
+          userService.hasOnboarded = true;
+          expect( userService )
+            .to.have.property( "hasOnboarded" )
+            .that.is.a( "boolean" )
+            .that.equals( false );
+        });
+      });
+    });
+
+    suite( "Events", function() {
+      suite( "edLogin", function() {
+        test( "should fire when login method is called with valid username and password", function() {
+
+        });
+
+        test( "should NOT fire when login actions are silently performed by EDWebSocket re-authentication", function() {
+
+        });
+
+        test( "should store authentication information for EDWebSocket re-authentication", function() {
+
+        });
+      });
+
+      suite( "edLogout", function () {
+        test( "should fire when user logs out", function () {
+
+        });
+      });
+    });
+
+    suite( "Methods", function() {
+      suite( "login", function() {
+        test("", function() {
+
+        });
+      });
+
+      suite( "logout", function() {
+        test( "when fired, should reset properties to default values", function() {
+
+        });
+
+        test( "returns true on successful logout", function() {
+
+        });
+
+        test( "returns false if user is still logged in", function() {
+
+        });
+      });
+
+      suite( "changeProfileImage", function() {
+        test( "returns EDUser object with new image information", function() {
+
+        });
+      });
     });
   });
 })( window, document, window.System, window.sinon, window.chai.expect );
