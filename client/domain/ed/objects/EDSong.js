af1ca0e7a42ad85a026713aaf4c0c49a1dcd7279
randy rollog <rrollog@eardish.com>
2015-03-30 12:18:32 -0700

    [#252] started getArtist method

--- client/domain/ed/objects/EDSong.js
+++ client/domain/ed/objects/EDSong.js
@@ -1,5 +1,6 @@
 import define from "domain/ed/define-properties";
 import EDMedia from "domain/ed/objects/EDMedia";
+//import edConnectionService from "domain/ed/services/ed-connection-service";
 
 export default class EDSong extends EDMedia {
   constructor( args ) {
@@ -16,6 +17,10 @@ export default class EDSong extends EDMedia {
   }
 
   getArtist() {
+    var json = {
+
+    };
+    return edConnectionService.formattedRequest;
   }
 
   getCreator() {
