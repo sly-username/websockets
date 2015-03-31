2537b61ce86baebc1c30e5b99bb3abf1c5d17118
randy rollog <rrollog@eardish.com>
2015-03-30 17:32:19 -0700

    [#325] added player methods

--- client/domain/ed/services/ed-player-service.js
+++ client/domain/ed/services/ed-player-service.js
@@ -1,7 +1,9 @@
+//import EDSong from "domain/ed/objects/EDSong";
+//import EDCollection from "domain/ed/objects/EDCollection";
 import EventEmitter from "domain/lib/eventEventEmitter";
 import createEvent from "domain/lib/event/create-event";
 import edAnalyticsService from "domain/analytics/EDAnalytics";
-//import EDSong from "domain/ed/objects/EDSong";
+//import edConnectionService from "domain/ed/services/ed-connection-service";
 
 var edPlayerService = new EventEmitter([
     "edPlay",
@@ -13,29 +15,53 @@ var edPlayerService = new EventEmitter([
     "edSkip",
     "edSkipTo"
   ]),
-  playerStats = null,
-  playerQueue = [];
+  currentStats = null,
+  queue = [];
 
 Object.defineProperties( edPlayerService, {
   currentStats: {
     configurable: false,
     enumerable: false,
     get: function() {
-      return playerStats;
+      return currentStats;
     }
   },
   queue: {
     configurable: false,
     enumerable: false,
     get: function() {
-      return playerQueue;
+      return queue;
     }
   }
 });
 
-edPlayerService.play = function( edSong ) {
-  return edConnectionService.formattedRequest( json )
-    .then(function( raw ) {
-      //return
-    });
+edPlayerService.play = function( EDSong ) {
+  if ( !currentStats.playing ) {
+    EDSong.play();
+    currentStats.playing = true;
+  }
 };
+
+edPlayerService.pause = function( EDSong ) {
+  if ( currentStats.playing ) {
+    EDSong.pause();
+    currentStats.playing = false;
+  }
+};
+
+edPlayerService.stop = function( EDSong ) {
+  if ( currentStats.playing ) {
+    EDSong.stop();
+    currentStats.playing = false;
+  }
+};
+
+edPlayerService.enqueue = function( EDSong ) {};
+
+edPlayerService.enqueueNext = function( EDSong ) {};
+
+edPlayerService.next = function() {};
+
+edPlayerService.skip = function() {};
+
+edPlayerService.skipTo = function( index ) {};
