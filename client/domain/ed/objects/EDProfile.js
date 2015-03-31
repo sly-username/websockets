c9defce85b6cc3a1a1162f99006c0ca5d10e648a
sly <sylvia@eardish.com>
2015-03-30 19:42:43 -0700

    [#326] Well, tests are passing.

--- client/domain/ed/objects/EDProfile.js
+++ client/domain/ed/objects/EDProfile.js
@@ -5,17 +5,6 @@ export default class EDProfile extends EDDataObject {
   constructor( args ) {
     super( args );
 
-    var badgesEarned = [],
-      Struct = BadgePair => {
-        Object.keys( BadgePair ).forEach( key => {
-          Object.defineProperty( this, key, {
-            value: BadgePair[ key ]
-          });
-          badgesEarned.push( key );
-        });
-        return new Struct( BadgePair );
-      };
-
     define.readOnly( this, [
       "userId",
       "email",
