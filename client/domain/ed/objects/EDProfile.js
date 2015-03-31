c3d0d3917e4a3dcdac0c83e5a15114542dbd6554
sly <sylvia@eardish.com>
2015-03-30 18:00:25 -0700

    [#326] Beginning writing tests.
    Not sure what to do about badgesEarned at the moment.

--- client/domain/ed/objects/EDProfile.js
+++ client/domain/ed/objects/EDProfile.js
@@ -4,17 +4,14 @@ import EDDataObject from "domain/ed/objects/EDDataObject";
 export default class EDProfile extends EDDataObject {
   constructor( args ) {
     super( args );
-    define.configRO( this, [
+    define.readOnly( this, [
+      "userId",
+      "email",
       "zipcode",
       "displayName",
-      "birthday",
-      "bio",
-      "website",
-      "yearFounded"
+      "createdDate",
+      "modifiedDate"
     ], args );
-    define.configRODeep( this, [ "name" ], args );
-    // should i define it this way to account for name having its own set of key/value pairs?
-    define.readOnly( this, [ "userId", "createdDate" ], args );
-    define.readOnlyDeep( this, [ "badgesEarned" ], args );
+    define.readOnlyDeep( this, "badgesEarned", args );
   }
 }
--- tests/domain/ed/objects/EDProfile.tests.js
+++ tests/domain/ed/objects/EDProfile.tests.js
@@ -1,3 +1,78 @@
-/**
- * Created by shwang on 3/27/15.
- */
+( function( win, doc, System, sinon, expect ) {
+  "use strict";
+
+  suite( "EDProfile", function() {
+    var EDProfile;
+
+    suiteSetup( function( done ) {
+      System.import( "domain/ed/objects/EDProfile" )
+        .then( function( imported ) {
+          EDProfile = imported.default;
+          done();
+        }, function( error ) {
+          console.warn( "Could not import 'EDProfile' for testing: ", error.message );
+          console.error( error.stack );
+          done( error );
+        });
+    });
+
+    suite( "EDProfile creation", function() {
+      suite( "Properties", function() {
+        test( "EDProfile has the properties specified in the constructor", function() {
+          var args = {
+              badgesEarned: {
+                test: null
+              }
+            },
+            edProfile = new EDProfile( args );
+
+          expect( edProfile )
+            .to.have.property( "userId" );
+
+          expect( edProfile )
+            .to.have.property( "email" );
+
+          expect( edProfile )
+            .to.have.property( "zipcode" );
+
+          expect( edProfile )
+            .to.have.property( "displayName" );
+
+          expect( edProfile )
+            .to.have.property( "createdDate" );
+
+          expect( edProfile )
+            .to.have.property( "modifiedDate" );
+
+          expect( edProfile )
+            .to.have.property( "badgesEarned" );
+        });
+      });
+
+      suite( "BadgesEarned", function() {
+        test( "badgesEarned is an array", function() {
+          var args = {},
+            edProfile = new EDProfile( args );
+
+          expect( edProfile )
+            .to.have.property( "badgesEarned" )
+            .that.is.an( "array" );
+        });
+
+        test( "BadgePair is an object", function() {
+          var args = {},
+            BadgePair = {
+              badgeId: 1,
+              dataAcquired: Date
+            },
+            edProfile = new EDProfile( args );
+
+          expect( edProfile )
+            .to.have.property( "badgesEarned" )
+            .that.deep.equals( "BadgePair" )
+            .that.is.an( "object" );
+        });
+      });
+    });
+  });
+})( window, document, window.System, window.sinon, window.chai.expect );
