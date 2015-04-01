168a741d9592deee2f650f6e35f0353271e82c1e
sly <sylvia@eardish.com>
2015-04-01 15:48:12 -0700

    [#329] Here goes nothing ...
    My favorite genre is country.

---
+++ client/domain/ed/objects/EDGenre.js
@@ -0,0 +1,9 @@
+import define from "domain/ed/define-properties";
+import EDDataObject from "domain/ed/objects/EDDataObject";
+
+export default class EDGenre extends EDDataObject {
+  constructor( args ) {
+    super( args );
+    define.readOnly( this, [ "id", "type", "name" ], args );
+  }
+}
