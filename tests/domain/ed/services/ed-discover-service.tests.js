dd6f4422fc806df36a2d328d385fe98aa7a54622
sly <sylvia@eardish.com>
2015-04-02 17:23:44 -0700

    [#324] Wrote tests for properties and passing parameters to getDiscoverSongList method.

--- tests/domain/ed/services/ed-discover-service.tests.js
+++ tests/domain/ed/services/ed-discover-service.tests.js
@@ -2,18 +2,20 @@
   "use strict";
 
   suite( "edDiscoverService", function() {
-    var edDiscoverService, EventEmitter, createEvent;
+    var edDiscoverService, edUserService, EDGenre;
 
     suiteSetup( function( done ) {
       Promise.all( [
         System.import( "domain/ed/services/ed-discover-service" ),
+        System.import( "domain/ed/services/ed-user-service" ),
         System.import( "domain/lib/event/EventEmitter" ),
         System.import( "domain/lib/event/create-event" )
       ] )
         .then( function( imported ) {
           edDiscoverService = imported[0].default;
-          EventEmitter = imported[1].default;
-          createEvent = imported[2].default;
+          edUserService = imported[1].default;
+          EventEmitter = imported[2].default;
+          createEvent = imported[3].default;
           done();
         }, function( error ) {
           console.warn( "Could not import 'edDiscoverService' for testing: ", error.message );
@@ -25,28 +27,51 @@
 // Tests begin
     suite( "Properties", function() {
       suite( "currentProfileBlend", function() {
-        test( "is an array", function() {
-
+        test( "edService has 'currentProfileBlend' property", function() {
+          expect( edDiscoverService )
+            .to.have.property( "currentProfileBlend" );
         });
 
         test( "is an array of EDGenre objects", function() {
-
+          expect( edDiscoverService.currentProfileBlend )
+            .to.be.an( "array" );
+            // .that.deep.equals( "i have no idea what" );
         });
       });
     });
 
     suite( "Methods", function() {
       suite( "getDiscoverSongList", function() {
-        test( "can only take in EDGenre object or 'blend' as parameters", function() {
+        test( "can take in EDGenre object as a parameter", function() {
+          var genreData = {
+                name: "bluegrass"
+              },
+            edGenre = new EDGenre( genreData ),
+            valid = edDiscoverService.getDiscoverSongList( edGenre );
 
+          expect( valid )
+            .to.be( "ok" );
+          // not sure what to test
+        });
+
+        test( "can take in 'blend' as a parameter", function() {
+          var valid = edDiscoverService.getDiscoverSongList( "blend" );
+
+          expect( valid )
+            .to.be( "ok" );
+          // yup, I got nothing.
+        });
+
+        test( "when invalid parameter is used, an error is thrown", function() {
+          var badParameter = "UNACCEPTABLE!",
+            invalid = edDiscoverService.getDiscoverSongList( badParameter );
+
+          expect( invalid )
+            .to.throw( Error );
         });
 
         suite( "blend parameter", function() {
-          test( "blend is a string", function() {
-
-          });
-
-          test( "when sending server blend request, server returns an array of genreIDs", function() {
+          test( "when sending server 'blend' request, if successful, server returns an array of genreIDs", function() {
 
           });
 
@@ -57,7 +82,6 @@
           test( "when server responds with songIDs, all songs are pushed to single songList array", function() {
 
           });
-
         });
 
         suite( "EDGenre parameter", function() {
