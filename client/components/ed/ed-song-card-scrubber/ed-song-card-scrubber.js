154c3eabd7da73163c2b2c897fdbc24882fa68b2
Khoa Ngo <khoa@eardish.com>
2015-04-06 15:59:45 -0700

    [#328] scrubber functional

--- client/components/ed/ed-song-card-scrubber/ed-song-card-scrubber.js
+++ client/components/ed/ed-song-card-scrubber/ed-song-card-scrubber.js
@@ -3,39 +3,65 @@
 
   polymer( "ed-song-card-scrubber", {
     ready: function() {
-      this.back = this.shadowRoot.getElementById( "back-circle" );
+      this.mouseDown = false;
+
+      // Selectors
       this.mid = this.shadowRoot.getElementById( "mid-circle" );
       this.front = this.shadowRoot.getElementById( "front-circle" );
       this.scrubber = this.shadowRoot.getElementById( "circle-scrubber" );
-      this.hiddenInput = this.shadowRoot.getElementById( "hidden-input" );
-      this.scrubCenter = [ ( this.scrubber.offsetLeft + ( this.scrubber.offsetWidth / 2 ) ),
-        ( this.scrubber.offsetTop + ( this.scrubber.offsetHeight / 2 ) ) ];
-      this.circFront = ( ( 2 * Math.PI ) * ( parseInt( this.front.getAttribute( "r" ), 10 )));
-      this.circMid = ( ( 2 * Math.PI ) * ( parseInt( this.mid.getAttribute( "r" ), 10 )));
-      this.bottomVal = parseInt( ( this.hiddenInput.max - this.hiddenInput.min ), 10 );
+      this.svgBox = this.scrubber.getBoundingClientRect();
+
+      // Calculates the center for the scrubber
+      this.scrubCenter = [ ( this.svgBox.left + ( this.svgBox.width / 2 ) ),
+        ( this.svgBox.top + ( this.svgBox.height / 2 ) ) ];
+
+      // Calculates the circumference of circles
+      this.circFront = ( 2 * Math.PI * ( parseInt( this.front.getAttribute( "r" ), 10 )));
+      this.circMid = ( 2 * Math.PI * ( parseInt( this.mid.getAttribute( "r" ), 10 )));
+      this.front.style[ "stroke-dasharray" ] = this.circFront + "%";
+      this.mid.style[ "stroke-dasharray" ] = this.circMid + "%";
     },
     attached: function() {
-      this.hiddenInput.addEventListener( "change", this.moveScrubber.bind( this ) );
-      // need to incorporate mousedown and up triggers
-      this.scrubber.addEventListener( "mousemove", this.triggerMove.bind( this ) );
+      // mouse events
+      this.scrubber.addEventListener( "mousedown", function() { this.mouseDown = true; }.bind( this ));
+      this.addEventListener( "mouseup", function() { this.mouseDown = false; }.bind( this ));
+      this.addEventListener( "mousemove", this.triggerMove.bind( this ) );
+      // touch events
+      this.scrubber.addEventListener( "touchstart", function() { this.mouseDown = true; }.bind( this ));
+      this.addEventListener( "touchend", function() { this.mouseDown = false; }.bind( this ));
+      this.addEventListener( "touchmove", this.triggerMove.bind( this ) );
     },
-    // logic is incorrect need to fix
+    // TODO Event listener may not be removed as expected
+    detached: function() {
+      this.scrubber.removeEventListener( "mousedown", function() { this.mouseDown = true; }.bind( this ));
+      this.removeEventListener( "mouseup", function() { this.mouseDown = false; }.bind( this ));
+      this.removeEventListener( "mousemove", this.triggerMove.bind( this ) );
+      // touch events
+      this.scrubber.removeEventListener( "touchstart", function() { this.mouseDown = true; }.bind( this ));
+      this.removeEventListener( "touchend", function() { this.mouseDown = false; }.bind( this ));
+      this.removeEventListener( "touchmove", this.triggerMove.bind( this ) );
+    },
     triggerMove: function( e ) {
-      var angle = Math.atan2( e.pageX - this.scrubCenter[0], -1 * ( e.pageY - this.scrubCenter[1] ) ) * ( 180 / Math.PI );
-      this.scrubber.style.webkitTransform = "rotate(" + angle + "deg)";
-      this.scrubber.style.transform = "rotate(" + angle + "deg)";
-    },
-    moveScrubber: function() {
-      this.topVal = parseInt( ( this.hiddenInput.value - this.hiddenInput.min ), 10 );
-      this.valPercent = parseInt( ( this.topVal / this.bottomVal ) * 100, 10 );
-      this.degPercent = parseInt( ( this.valPercent * 360 ) / 100, 10 );
+      var angle,
+        radians,
+        currentVal;
 
-      this.front.style[ "stroke-dasharray" ] = this.circFront + "%";
-      this.front.style[ "stroke-dashoffset" ] = ( this.circFront - ( ( this.valPercent * this.circFront ) / 100 ) ) + "%";
-      this.mid.style[ "stroke-dasharray" ] = this.circMid + "%";
-      this.mid.style[ "stroke-dashoffset" ] = ( this.circMid - ( ( this.valPercent * this.circMid ) / 100 ) ) + "%";
-      this.scrubber.style.webkitTransform = "rotate(" + ( this.degPercent - 92 ) + "deg)";
-      this.scrubber.style.transform = "rotate(" + ( this.degPercent - 92 ) + "deg)";
+      if ( this.mouseDown ) {
+        radians = Math.atan2( e.pageX - this.scrubCenter[ 0 ], e.pageY - this.scrubCenter[ 1 ] );
+        angle = ( radians * ( 180 / Math.PI ) * -1 ) + 90;
+        currentVal = ( angle * this.max ) / 360;
+        this.scrubber.style.webkitTransform = "rotate(" + angle + "deg)";
+        this.scrubber.style.transform = "rotate(" + angle + "deg)";
+        this.front.style[ "stroke-dashoffset" ] = ( ( ( -1 * angle * this.circFront ) / 360 ) - ( this.circFront * 1.25 ) ) + "%";
+        this.mid.style[ "stroke-dashoffset" ] = ( ( ( -1 * angle * this.circMid ) / 360 ) - ( this.circMid * 1.25 ) ) + "%";
+      }
+    },
+    attributeChanged: function() {
+      var degPercent = parseInt( ( this.value / this.max ) * 360, 10 );
+      this.scrubber.style.webkitTransform = "rotate(" + ( degPercent - 90 ) + "deg)";
+      this.scrubber.style.transform = "rotate(" + ( degPercent - 90 ) + "deg)";
+      this.front.style[ "stroke-dashoffset" ] = ( ( ( -1 * ( degPercent - 90 ) * this.circFront ) / 360 ) - ( this.circFront * 1.25 ) ) + "%";
+      this.mid.style[ "stroke-dashoffset" ] = ( ( ( -1 * ( degPercent - 90 ) * this.circMid ) / 360 ) - ( this.circMid * 1.25 ) ) + "%";
     }
   });
 })( window.Polymer );
