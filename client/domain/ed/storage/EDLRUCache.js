d6236039f9d57fa948712d55c0453e87e165a6af
randy rollog <rrollog@eardish.com>
2015-04-06 14:21:46 -0700

    [#319] modified super methods to check keys as instances of EDDataObj

--- client/domain/ed/storage/EDLRUCache.js
+++ client/domain/ed/storage/EDLRUCache.js
@@ -1,14 +1,50 @@
 import EDDataObject from "domain/ed/objects/EDDataObject";
 import ObservableLRUCache from "domain/lib/storage/ObservableLRUCache";
 
+var checkKeyType;
+
+// helpers
+checkKeyType = function( tmpKey ) {
+  tmpKey = tmpKey instanceof EDDataObject ? tmpKey.id : tmpKey;
+
+  return tmpKey;
+};
+
 export default class EDLRUCache extends ObservableLRUCache {
   constructor( limit ) {
     super( limit );
   }
 
-  set( edDataObject ) {
-    if ( !( edDataObject instanceof EDDataObject ) ) {
-      throw new TypeError( "cannot pass a non-EDArtist instance" );
+  set( key, data ) {
+    if ( key instanceof EDDataObject ) {
+      key = key.id;
+      data = key;
     }
+
+    return super.set( key, data );
+  }
+
+  get( key ) {
+    key = checkKeyType( key );
+
+    return super.get( key );
+  }
+
+  peek( key ) {
+    key = checkKeyType( key );
+
+    return super.peek( key );
+  }
+
+  has( key ) {
+    key = checkKeyType( key );
+
+    return super.has( key );
+  }
+
+  remove( key ) {
+    key = checkKeyType( key );
+
+    return super.remove( key );
   }
 }
