79a8dbbbe651d25f6cfa7e9e62afec32b2e55e24
randy rollog <rrollog@eardish.com>
2015-04-06 17:38:04 -0700

    [#319] initial testing for edLRU methods

--- client/domain/ed/storage/EDLRUCache.js
+++ client/domain/ed/storage/EDLRUCache.js
@@ -1,13 +1,11 @@
+/*jshint strict: false */
+/*eslint vars-on-top: 0*/
 import EDDataObject from "domain/ed/objects/EDDataObject";
 import ObservableLRUCache from "domain/lib/storage/ObservableLRUCache";
 
-var checkKeyType;
-
 // helpers
-checkKeyType = function( tmpKey ) {
-  tmpKey = tmpKey instanceof EDDataObject ? tmpKey.id : tmpKey;
-
-  return tmpKey;
+var checkKeyType = function( tmpKey ) {
+  return tmpKey instanceof EDDataObject ? tmpKey.id : tmpKey;
 };
 
 export default class EDLRUCache extends ObservableLRUCache {
@@ -15,18 +13,53 @@ export default class EDLRUCache extends ObservableLRUCache {
     super( limit );
   }
 
+  /**
+   * @method observe
+   * @param key -- the key to observe on
+   * @param callback {observerCallback} -- observer function
+   * @param acceptList { Array<string> } -- list of actions to observe, accepted: "set" and "remove"
+   * @returns {undefined}
+   *
+   * @description
+   *    Checks if key passed is an instance of EDDataObject and if so,
+   *    assigns key to the id of that instance. It then adds an observer
+   *    callback for watching for changes to a specific key. Changes
+   *    are only observed via set and remove operations, reordering
+   *    an item in the internal linked list is not observable.
+   */
   observe( key, callback, acceptList ) {
     key = checkKeyType( key );
 
     return super.observe( key, callback, acceptList );
   }
 
+  /**
+   *
+   * @param key -- the key the callback is attached to
+   * @param callback {function} -- the observer function to remove
+   * @returns {undefined}
+   *
+   * @description
+   *   Checks if key passed is an instance of EDDataObject and if so,
+   *   assigns key to the id of that instance. Removes a listener from
+   *   the internal list of observers.
+   */
   unobserve( key, callback ) {
     key = checkKeyType( key );
 
     return super.unobserve( key, callback );
   }
 
+  /**
+   *
+   * @param key -- the key the callback is attached to
+   * @param data
+   * @returns {*}
+   *
+   * @description
+   *   Checks if key passed is an instance of EDDataObject and if so,
+   *   assigns key to the id of that instance.
+   */
   set( key, data ) {
     if ( key instanceof EDDataObject ) {
       key = key.id;
--- tests/domain/ed/storage/EDLRUCache.tests.js
+++ tests/domain/ed/storage/EDLRUCache.tests.js
@@ -4,7 +4,7 @@
   "use strict";
 
   suite( "EDLRUCache", function() {
-    var EDLRUCache;
+    var EDLRUCache, EDArtist;
 
     suiteSetup( function( done ) {
       System.import( "domain/ed/storage/EDLRUCache" )
@@ -16,21 +16,173 @@
           console.error( error.stack );
           done( error );
         });
+
+      System.import( "domain/ed/objects/EDArtist" )
+        .then( function( imported ) {
+          EDArtist = imported.default;
+          done();
+        }, function( error ) {
+          console.warn( "Could not import 'EDLRUCache' for testing: ", error.message );
+          console.error( error.stack );
+          done( error );
+        });
     });
 
     suite( "EDLRUCache creation", function() {
-      test( "constructor has expected properties", function() {
+      test( "instance has expected properties & methods", function() {
         var edCache = new EDLRUCache( 10 );
 
         expect( edCache )
-          .to.have.property( "limit" )
-          .to.be.a( "number" )
-          .that.equals( 10 );
+          .to.have.property( "observe" )
+          .to.be.a( "function" );
 
         expect( edCache )
-          .to.have.property( "size" )
-          .to.be.a( "number" )
-          .that.equals( 0 );
+          .to.have.property( "unobserve" )
+          .to.be.a( "function" );
+
+        expect( edCache )
+          .to.have.property( "set" )
+          .to.be.a( "function" );
+
+        expect( edCache )
+          .to.have.property( "get" )
+          .to.be.a( "function" );
+
+        expect( edCache )
+          .to.have.property( "peek" )
+          .to.be.a( "function" );
+
+        expect( edCache )
+          .to.have.property( "has" )
+          .to.be.a( "function" );
+
+        expect( edCache )
+          .to.have.property( "remove" )
+          .to.be.a( "function" );
+      });
+    });
+
+    suite( "Class Methods", function() {
+      test( "observe method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          acceptList = [ "set", "remove" ],
+          observeCbSpy = sinon.spy(),
+          observeSpy = sinon.spy( edCache, "observe" );
+
+        edCache.observe( edArtist, observeCbSpy, acceptList );
+
+        expect( observeSpy )
+          .to.have.callCount( 1 );
+
+        observeSpy.restore();
+      });
+
+      test( "unobserve method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          unobserveCbSpy = sinon.spy(),
+          unobserveSpy = sinon.spy( edCache, "unobserve" );
+
+        edCache.unobserve( edArtist, unobserveCbSpy );
+
+        expect( unobserveSpy )
+          .to.have.callCount( 1 );
+
+        unobserveSpy.restore();
+      });
+
+      test( "set method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          setSpy = sinon.spy( edCache, "set" );
+
+        edCache.set( edArtist );
+
+        expect( setSpy )
+          .to.have.callCount( 1 );
+
+        setSpy.restore();
+      });
+
+      test( "get method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          getSpy = sinon.spy( edCache, "get" );
+
+        edCache.get( edArtist );
+
+        expect( getSpy )
+          .to.have.callCount( 1 );
+
+        getSpy.restore();
+      });
+
+      test( "peek method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          peekSpy = sinon.spy( edCache, "peek" );
+
+        edCache.peek( edArtist );
+
+        expect( peekSpy )
+          .to.have.callCount( 1 );
+
+        peekSpy.restore();
+      });
+
+      test( "has method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          hasSpy = sinon.spy( edCache, "has" );
+
+        edCache.has( edArtist );
+
+        expect( hasSpy )
+          .to.have.callCount( 1 );
+
+        hasSpy.restore();
+      });
+
+      test( "remove method", function() {
+        var artistObj = {
+            id: "001",
+            yearFounded: 2009
+          },
+          edCache = new EDLRUCache( 10 ),
+          edArtist = new EDArtist( artistObj ),
+          removeSpy = sinon.spy( edCache, "remove" );
+
+        edCache.remove( edArtist );
+
+        expect( removeSpy )
+          .to.have.callCount( 1 );
+
+        removeSpy.restore();
       });
     });
   });
