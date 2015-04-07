782466a6e2ed071a4ed1a2d5a665526aa852e388
sly <sylvia@eardish.com>
2015-04-07 13:47:09 -0700

    [#364] Wrote the get by index function.
    Before writing tests. But needed to figure out what was supposed to happen and what it was supposed to return. I think it's clearer now.

--- client/domain/ed/storage/EDCollection.js
+++ client/domain/ed/storage/EDCollection.js
@@ -1,13 +1,35 @@
-var iterator = Symbol( "iterator" ),
-  ids = [];
+//import edDataService from "domain/ed/services/ed-data-service";
+
+var iterator = Symbol( "iterator" );
 
 export default class EDCollection {
   constructor( ids, type ) {
+    this.ids = ids;
+    // should ids be private?
+    this.type = type;
 
   }
 
   get( index ) {
+    if ( this.ids[ index ] instanceof EDDataObject ) {
+      return ids[ index ];
+    }
 
+    if ( typeof this.ids[ index ] !== "string" ) {
+      Promise.resolve( this.ids[ index ] )
+        .then( val => {
+          return edDataService.getDataObjectById( val )
+            // TODO made up function name
+            .then( dataObj => {
+              return this.ids[ index ] = dataObj;
+            });
+        });
+    }
+
+    return edDataService.getDataObjectById( this.ids[ index ] )
+      .then( function( dataObj ) {
+        return this.ids[ index ] = dataObj;
+      });
   }
 
   getRange( indexFrom, indexTo ) {
