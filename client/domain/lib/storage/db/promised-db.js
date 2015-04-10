531e0791773ffb24a1ed042ba16140359948b95b
Ryan Bogle <ryan@eardish.com>
2015-04-09 22:22:54 -0700

    [#245] Yay for failing tests!
    Things are coming along, still need more time, found out I can't use
    proxies so I have to go the long way around to implement the API I have
    been planning.
    
    Tests are working properly and fairly in depth so development should be
    fairly swift now. Need to switch the object store methods to create new
    transactions. Then I will implement the API for object store indexes.

--- client/domain/lib/storage/db/promised-db.js
+++ client/domain/lib/storage/db/promised-db.js
@@ -25,52 +25,310 @@ promisedDB.open( "testing-track-db", 1, [
 ]);
 */
 
-var indexedDB = window.indexedDB ||
-      window.webkitIndexedDB ||
-      window.mozIndexedDB ||
-      window.msIndexedDB,
+var
+  dbSymbol = Symbol( "internalDB" ),
+  objectStoreSymbol = Symbol( "internalObjectStore" ),
+  indexedDB,
+  createUpgradeHandler,
   createObjectStoreInDB,
   createIndexOnObjectStore;
 
-class PromisedDB {
-  constructor( name, version, config ) {}
+// Find the right indexedDB reference
+indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+
+// PDBObjectStore wrapping the IDBObjectStore
+class PDBObjectStore {
+  constructor( idbObjectStore ) {
+    Object.defineProperties( this, {
+      [dbSymbol]: {
+        configurable: false,
+        enumerable: false,
+        writeable: false,
+        value: idbObjectStore.transaction.db
+      },
+      [objectStoreSymbol]: {
+        configurable: false,
+        enumerable: false,
+        writeable: false,
+        value: idbObjectStore
+      }
+    });
+
+    console.log( "in PDBOjectStore[[construct]] %o", this );
+  }
+
+  get indexNames() {
+    return this[ objectStoreSymbol ].indexNames;
+  }
+
+  get keyPath() {
+    return this[ objectStoreSymbol ].keyPath;
+  }
+
+  get name() {
+    return this[ objectStoreSymbol ].name;
+  }
+
+  get transaction() {
+    return this[ objectStoreSymbol ].transaction;
+  }
+
+  get autoIncrement() {
+    return this[ objectStoreSymbol ].autoIncrement;
+  }
+
+  add( value, key ) {
+    return new Promise(( resolve, reject ) => {
+      var request;
+
+      if ( arguments.length === 2 ) {
+        request = this[ objectStoreSymbol ].add( value, key );
+      } else {
+        request = this[ objectStoreSymbol ].add( value );
+      }
+
+      request.onsuccess = function( event ) {
+        console.log( "add success, %o", event );
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  clear() {
+    return new Promise(( resolve, reject ) => {
+      var request = this[ objectStoreSymbol ].clear();
+
+      request.onsuccess = function( event ) {
+        console.log( "clear success, %o", event );
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  delete( key ) {
+    return new Promise(( resolve, reject ) => {
+      var request = this[ objectStoreSymbol ].delete( key );
+
+      request.onsuccess = function( event ) {
+        console.log( "delete success, %o", event );
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  get( key ) {
+    return new Promise(( resolve, reject ) => {
+      var request = this[ objectStoreSymbol ].get( key );
+
+      request.onsuccess = function( event ) {
+        console.log( "get success, %o", event );
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  index( name ) {
+    return this[ objectStoreSymbol ].index( name );
+  }
+
+  put( value, optionalKey ) {
+    return new Promise(( resolve, reject ) => {
+      var request;
+
+      if ( arguments.length === 2 ) {
+        request = this[ objectStoreSymbol ].put( value, optionalKey );
+      } else {
+        request = this[ objectStoreSymbol ].put( value );
+      }
+
+      request.onsuccess = function( event ) {
+        console.log( "put success, %o", event );
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  openCursor( range, direction ) {
+    return new Promise(( resolve, reject ) => {
+      var request = this[ objectStoreSymbol ].openCursor( range, direction );
+
+      request.onsuccess = function( event ) {
+        resolve( event.target.result );
+      };
+
+      request.onerror = function( event ) {
+        reject( event );
+      };
+    });
+  }
+
+  count( range ) {
+    var request;
+
+    if ( range != null ) {
+      request = this[ objectStoreSymbol ].count( range );
+    } else {
+      request = this[ objectStoreSymbol ].count();
+    }
+
+    request.onsuccess = function( event ) {
+      resolve( event.target.result );
+    };
+
+    request.onerror = function( event ) {
+      reject( event );
+    };
+  }
 }
+
+// Main class for PromisedDB instance
+class PromisedDB {
+  constructor( db ) {
+    Object.defineProperty( this, dbSymbol, {
+      configurable: false,
+      enumerable: false,
+      writeable: false,
+      value: db
+    });
+
+    Array.from( db.objectStoreNames ).forEach( storeName => {
+      Object.defineProperty( this, storeName, {
+        configurable: false,
+        enumberable: false,
+        writeable: false,
+        value: new PDBObjectStore(
+          db.transaction( storeName, "readwrite" ).objectStore( storeName )
+        )
+      });
+    });
+  }
+
+  get name() {
+    return this[ dbSymbol ].name;
+  }
+
+  get version() {
+    return this[ dbSymbol ].version;
+  }
+
+  get objectStoreNames() {
+    return this[ dbSymbol ].objectStoreNames;
+  }
+
+  close() {
+    return this[ dbSymbol ].close();
+  }
+}
+
+// creates and returns an object store
+createObjectStoreInDB = function( indexedDBInstance, storeName, optionalParameters={} ) {
+  return indexedDBInstance.createObjectStore( storeName, optionalParameters );
+};
+
+// creates and returns an index
+createIndexOnObjectStore = function( objectStore, indexName, keyPath, optionalParameters={} ) {
+  return objectStore.createIndex( indexName, keyPath, optionalParameters );
+};
+
+// Generates a function that is used as the onupgradeneeded event handler
+//  for the given config array and version number
+createUpgradeHandler = function( currentVersion, configArray ) {
+  return function( upgradeEvent ) {
+    var idb = upgradeEvent.target.result;
+
+    configArray.forEach(function( storeConfig, index ) {
+      // check version
+      if ( idb.version > index + 1 ) {
+        // skip this version (it is already set up)
+        console.log( "%s skipping version %d", idb.name, index + 1 );
+        return;
+      }
+
+      // Iterate over store configuration, getting each store name
+      Object.keys( storeConfig ).forEach(function( storeName ) {
+        var currentStoreConfig = storeConfig[ storeName ],
+          currentObjectStore =
+            createObjectStoreInDB(
+              idb,
+              storeName,
+              currentStoreConfig.options
+            );
+
+        // todo remove debug
+        console.log( "created store: " + currentObjectStore.name );
+
+        // Iterate over this store's index configuration, getting each index name
+        Object.keys( currentStoreConfig.indexes || {} ).forEach(function( indexName ) {
+          var indexConfig = currentStoreConfig.indexes[ indexName ],
+            currentIndex =
+              createIndexOnObjectStore(
+                currentObjectStore,
+                indexName,
+                ...indexConfig
+              );
+
+          // todo remove debug
+          console.log( "created index " + currentIndex.name + " on store " + currentObjectStore.name );
+        });
+      });
+    });
+  };
+};
 
 export default {
   open( name, version, config ) {
-    var openRequest = indexedDB.open( name, version ),
-      idb;
+    return new Promise(function( resolve, reject ) {
+      var openRequest = indexedDB.open( name, version );
 
-    openRequest.onupgradeneeded = function( upgradeEvent ) {
-      console.log( "onupgradeneeded happened, %o %d %o", name, version, config );
-      idb = upgradeEvent.target.result;
+      // perform upgrade
+      openRequest.onupgradeneeded = createUpgradeHandler( version, config );
 
-      // Iterates over the config array, index+1 == version number
-      config.forEach(function( storeConfig, index ) {
-        if ( version > index + 1 ) {
-          // Skip set up for this config
-          return;
-        }
+      // reject if blocked?
+      openRequest.onblocked = function( event ) {
+        console.log( name + " version: " + version + " openRequest.onblocked %o", event );
+        reject( event );
+      };
 
-        // Iterates over store configuration, getting each store name
-        Object.keys( storeConfig ).forEach(function( storeName ) {
-          var currentStore = idb.createObjectStore( storeName, storeConfig[ storeName ].options || null );
+      // create and resolve on success
+      openRequest.onsuccess = function( event ) {
+        console.log( name + " version: " + version + " openRequest.onsuccess %o", event );
+        resolve( new PromisedDB( event.target.result ) );
+      };
 
-          // Iterates over this store's index configuration, getting each index name
-          Object.keys( storeConfig[ storeName ].indexes || {} ).forEach(function( indexName ) {
-            console.log( currentStore.name + " " + indexName );
-            console.dir( [ indexName ].concat( storeConfig[ storeName ].indexes[ indexName ] ));
-            currentStore.createIndex.apply( currentStore, [ indexName ].concat( storeConfig[ storeName ].indexes[ indexName ]));
-          });
-        });
+      // reject on error
+      openRequest.onerror = function( event ) {
+        console.warn( name + " version: " + version + " openRequest.onerror %o", event );
+        reject( event );
+      };
+    })
+      .catch( error => {
+        console.log( "in promisedDB.open new promise catch" );
+        console.error( error );
+        // TODO?
+        return null;
       });
-    };
-
-
-
-    return Promise.resolve({});
   },
   deleteDatabase( name ) {
-    indexedDB.deleteDatabase( name );
+    return indexedDB.deleteDatabase( name );
   }
 };
--- tests/domain/lib/storage/db/promised-db.tests.js
+++ tests/domain/lib/storage/db/promised-db.tests.js
@@ -26,7 +26,7 @@
 
     // Start Test
     test( "construction with fully formed config object", function( done ) {
-      var db;
+      var dbOpenRequest;
 
       promisedDB.open( "testing-track-db", 1, [{
         storeName: {
@@ -49,34 +49,38 @@
             }]
           }
         }
-      }]).close();
+      }])
+        .then(function( pdb ) {
+          console.log( "testing-track-db constructed: %o", pdb );
+          pdb.close();
 
-      db = indexedDB.open( "testing-track-db", 1 );
-      db.onsuccess = function( event ) {
-        var idb = event.target.result,
-          transaction = idb.transaction([ "testing-track-db" ], "readonly" ),
-          store;
+          dbOpenRequest = indexedDB.open( "testing-track-db", 1 );
+          dbOpenRequest.onsuccess = function( event ) {
+            var idb = event.target.result,
+              transaction = idb.transaction( "storeName", "readonly" ),
+              store;
 
-        transaction.oncomplete = function( trans ) {
-          db.close();
-          done();
-        };
+            transaction.oncomplete = function() {
+              idb.close();
+              done();
+            };
 
-        store = transaction.objectStore( "storeName" );
+            store = transaction.objectStore( "storeName" );
 
-        expect( store )
-          .to.be.an.instanceOf( IDBObjectStore );
+            expect( store )
+              .to.be.an.instanceOf( IDBObjectStore );
 
-        expect( store )
-          .to.have.property( "name" )
-          .that.equals( "storeName" );
-      };
+            expect( store )
+              .to.have.property( "name" )
+              .that.equals( "storeName" );
+          };
 
-      promisedDB.deleteDatabase( "testing-track-db" );
+          promisedDB.deleteDatabase( "testing-track-db" );
+        });
     });
 
     test( "construction with minimal config object", function( done ) {
-      var db;
+      var dbOpenRequest;
 
       promisedDB.open( "testing-track-db-2", 1, [{
         storeName: {
@@ -86,83 +90,38 @@
             profileId: [ "profileId" ]
           }
         }
-      }]).close();
+      }]).then(function( pdb ) {
+        console.log( "testing-track-db-2 constructed: %o", pdb );
+        pdb.close();
 
-      db = indexedDB.open( "testing-track-db-2", 1 );
-      db.onsuccess = function( event ) {
-        var idb = event.target.result,
-          transaction = idb.transaction([ "testing-track-db-2" ], "readonly" ),
-          store;
+        dbOpenRequest = indexedDB.open( "testing-track-db-2", 1 );
+        dbOpenRequest.onsuccess = function( event ) {
+          var idb = event.target.result,
+            transaction = idb.transaction( "storeName", "readonly" ),
+            store;
 
-        transaction.oncomplete = function( trans ) {
-          db.close();
-          done();
+          transaction.oncomplete = function() {
+            idb.close();
+            done();
+          };
+
+          store = transaction.objectStore( "storeName" );
+
+          expect( store )
+            .to.be.an.instanceOf( IDBObjectStore );
+
+          expect( store )
+            .to.have.property( "name" )
+            .that.equals( "storeName" );
         };
 
-        store = transaction.objectStore( "storeName" );
-
-        expect( store )
-          .to.be.an.instanceOf( IDBObjectStore );
-
-        expect( store )
-          .to.have.property( "name" )
-          .that.equals( "storeName" );
-      };
-
-      promisedDB.deleteDatabase( "testing-track-db-2" );
+        promisedDB.deleteDatabase( "testing-track-db-2" );
+      });
     });
 
     suite( "Own Properties & Symbols", function() {
-      var db;
-
-      suiteSetup(function( done ) {
-        var data = [
-            {
-              id: 0,
-              type: "track",
-              userId: 0,
-              profileId: 0,
-              name: "After the Rain"
-            },
-            {
-              id: 1,
-              type: "track",
-              userId: 0,
-              profileId: 1,
-              name: "Recommendation"
-            },
-            {
-              id: 2,
-              type: "track",
-              userId: 1,
-              profileId: 2,
-              name: "Turn Left"
-            },
-            {
-              id: 3,
-              type: "track",
-              userId: 2,
-              profileId: 3,
-              name: "Blinking Pigs"
-            },
-            {
-              id: 4,
-              type: "track",
-              userId: 1,
-              profileId: 4,
-              name: "Howling at the Moon"
-            }
-          ],
-          doneHelper = (function() {
-            var count = 0;
-            return function() {
-              if ( ++count === 5 ) {
-                done();
-              }
-            };
-          })();
-
-        db = promisedDB.open( "testing-track-db", 1, [{
+      var pdb,
+        config = [{
           storeName: {
             options: {
               keyPath: "id",
@@ -183,40 +142,286 @@
               }]
             }
           }
-        }]);
+        }],
+        seedData = [
+          {
+            id: 0,
+            type: "track",
+            userId: 0,
+            profileId: 0,
+            name: "After the Rain"
+          },
+          {
+            id: 1,
+            type: "track",
+            userId: 0,
+            profileId: 1,
+            name: "Recommendation"
+          },
+          {
+            id: 2,
+            type: "track",
+            userId: 1,
+            profileId: 2,
+            name: "Turn Left"
+          },
+          {
+            id: 3,
+            type: "track",
+            userId: 2,
+            profileId: 3,
+            name: "Blinking Pigs"
+          },
+          {
+            id: 4,
+            type: "track",
+            userId: 1,
+            profileId: 4,
+            name: "Howling at the Moon"
+          }
+        ];
 
-        data.forEach(function( entry ) {
-          return db.objectStore.storeName.put( entry ).then( doneHelper );
+      suiteSetup(function( done ) {
+        var doneHelper = (function() {
+          var count = 0;
+          return function( arg ) {
+            console.log( "in done helper: %d, %o", count, arg );
+
+            if ( ++count === 5 ) {
+              done();
+            }
+          };
+        })();
+
+        promisedDB.open( "testing-track-db", 1, config ).then(function( createdPDB ) {
+          pdb = createdPDB;
+
+          seedData.forEach(function( entry ) {
+            return pdb.storeName.add( entry ).then( doneHelper );
+          });
         });
       });
 
       suiteTeardown(function() {
-        db.close();
-        promisedDB.deleteDatabase( db.name );
+        pdb.close();
+        promisedDB.deleteDatabase( pdb.name );
       });
 
-      test( "db has property name", function() {
-        expect( db )
+      test( "promised db has property name", function() {
+        expect( pdb )
           .to.have.property( "name" )
           .that.is.a( "string" )
           .and.equals( "testing-track-db" );
       });
 
-      test( "db has property version", function() {
-        expect( db )
+      test( "promised db has property version", function() {
+        expect( pdb )
           .to.have.property( "version" )
           .that.is.a( "number" )
           .and.equals( 1 );
       });
 
-      test( "db has property objectStore", function() {
-        expect( db )
-          .to.have.property( "objectStore" );
+      config.forEach(function( versionConfig, versionNumber ) {
+        versionNumber += 1;
 
-        expect( db.objectStore )
-          .to.have.property( "storeName" )
-          .to.have.property( "indexes" );
-      });
+        suite( "version number: " + versionNumber, function() {
+          Object.keys( versionConfig ).forEach(function( storeName ) {
+            suite( "objectStore: " + storeName, function() {
+              test( "pdb has property objectStore: " + storeName, function() {
+                expect( pdb )
+                  .to.have.property( storeName )
+                  .that.is.an.instanceof( PDBObjectStore ); // todo import this
+              });
+
+              suite( storeName + " has objectStore properties", function() {
+                test( "indexNames", function() {
+                  expect( pdb[ storeName ] )
+                    .to.have.property( "indexNames" )
+                    .that.is.an.instanceof( DOMStringList );
+                });
+
+                test( "keyPath", function() {
+                  expect( pdb[ storeName ] )
+                    .to.have.property( "keyPath" )
+                    .that.is.a( "string" )
+                    .and.equals( versionConfig[ storeName ].options.keyPath );
+                });
+
+                test( "name", function() {
+                  expect( pdb[ storeName ] )
+                    .to.have.property( "name" )
+                    .that.is.a( "string" )
+                    .and.equals( storeName );
+                });
+
+                test( "autoIncrement", function() {
+                  expect( pdb[ storeName ] )
+                    .to.have.property( "autoIncrement" )
+                    .that.equals( versionConfig[ storeName ].options.autoIncrement );
+                });
+              });
+
+              suite( storeName + " has objectStore methods", function() {
+                var newData = {
+                    id: 5,
+                    type: "track",
+                    userId: 3,
+                    profileId: 8,
+                    name: "Hunger of the Pine"
+                  },
+                  augmentedData = Object.assign( {}, seedData[1] );
+
+                augmentedData.userId = 10;
+
+                test( "add", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "add" );
+
+                  pdb[ storeName ].add( newData )
+                    .then(function( id ) {
+                      expect( id ).to.equal( 5 );
+                      done();
+                    }).catch( done );
+                });
+
+                test( "delete", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "delete" );
+
+                  pdb[ storeName ].delete( 5 )
+                    .then(function( data ) {
+                      // todo expect something
+                      done();
+                    })
+                    .catch( done );
+                });
+
+                test( "clear", function() {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "clear" );
+
+                  // todo, actually check if it works
+                });
+
+                test( "get", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "get" );
+
+                  pdb[ storeName ].get( 0 )
+                    .then(function( data ) {
+                      expect( data )
+                        .to.equal( seedData[ 0 ] );
+                      done();
+                    }).catch( done );
+                });
+
+                test( "put", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "put" );
+
+                  pdb[ storeName ].put( augmentedData )
+                    .then(function( data ) {
+                      expect( data )
+                        .to.equal( augmentedData.id );
+//                        .to.deep.equal( augmentedData );
+                      done();
+                    }).catch( done );
+                });
+
+                test( "openCursor", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "openCursor" );
+
+                  pdb[ storeName ].openCursor()
+                    .then(function( cursor ) {
+                      expect( cursor )
+                        .to.be.instanceof( IDBCursor );
+                    }).catch( done );
+                });
+
+                test( "count", function( done ) {
+                  expect( pdb[ storeName ] )
+                    .to.respondTo( "count" );
+
+                  pdb[ storeName ].count()
+                    .then(function( count ) {
+                      expect( count )
+                        .to.equal( seedData.length );
+                      done();
+                    }).catch( done );
+                });
+              });
+
+              suite( storeName + " has proper indexes", function() {
+                Object.keys( versionConfig[ storeName ].indexes ).forEach(function( indexName ) {
+                  suite( storeName + " index: " + indexName, function() {
+                    test( "object store: " + storeName + " has index " + indexName, function() {
+                      expect( pdb[ storeName ] )
+                        .to.have.property( indexName );
+                    });
+
+                    suite( indexName + " has index properties", function() {
+                      test( "name", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.have.property( "name" )
+                          .that.equals( indexName );
+                      });
+
+                      test( "objectStore", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.have.property( "objectStore" )
+                          .that.equals( pdb[ storeName ] );
+                      });
+
+                      test( "keyPath", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.have.property( "keyPath" );
+                      });
+
+                      test( "multiEntry", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.have.property( "multiEntry" );
+                      });
+
+                      test( "unique", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.have.property( "unique" );
+                      });
+                    });
+
+                    suite( indexName + " has index properties", function() {
+                      test( "count", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.respondTo( "count" );
+                      });
+
+                      test( "get", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.respondTo( "get" );
+                      });
+
+                      test( "getKey", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.respondTo( "getKey" );
+                      });
+
+                      test( "openCursor", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.respondTo( "openCursor" );
+                      });
+
+                      test( "openKeyCursor", function() {
+                        expect( pdb[ storeName ][ indexName ] )
+                          .to.respondTo( "openKeyCursor" );
+                      });
+                    });
+                  });
+                });
+              }); // end storeName has indexes suite
+            }); // end object store suite
+          }); // end versionConfig.keys.forEach
+        }); // end version suite
+      }); // end config.forEach
     });
 
     suite( "Methods", function() {});
