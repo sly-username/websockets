cdbbe12bb1ce50a3fc68615c9874766909a85f2c
Khoa Ngo <khoa@eardish.com>
2015-04-10 13:35:06 -0700

    [#328] fixed svgs form ed-icons

--- client/components/ed/ed-icon/ed-icon.js
+++ client/components/ed/ed-icon/ed-icon.js
@@ -11,8 +11,13 @@
         reflect: true
       }
     },
+    ready: function() {
+      this.triggerSVG = this.shadowRoot.getElementById( "svg-use" );
+      this.getUrl = "/assets/icons/svg.svg#" + this.name;
+    },
     attached: function() {
       this.shadowRoot.querySelector( "svg" ).onload = console.log.bind(console);
+      this.triggerSVG.setAttributeNS( "http://www.w3.org/1999/xlink", "xlink:href", this.getUrl );
     },
     rotationChanged: function( oldValue, newValue ) {
       switch ( newValue ) {
