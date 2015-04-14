cdb5a5d45fa7b163f92f8cd88c3c631ac123d30e
sly <sylvia@eardish.com>
2015-04-14 14:37:41 -0700

    [#373] Integrated user-service with login views.
    Added submit button event handler that uses user-service to log user in.

--- client/views/login/ed-login-view/ed-login-view.js
+++ client/views/login/ed-login-view/ed-login-view.js
@@ -1,13 +1,34 @@
-( function( polymer ) {
+( function( polymer, System ) {
   "use strict";
 
-  polymer( "ed-login-view", {
-    /* LIFECYCLE */
-    ready: function() {},
-    attached: function() {},
-    detached: function() {},
-    attributeChanged: function( attrName, oldValue, newValue ) {}
-    /* PROPERTIES */
-    /* METHODS */
-  });
-})( window.Polymer );
+  System.import( "domain/ed/services/ed-user-service" )
+    .then( function( imported ) {
+      var userService = imported.default;
+
+      polymer( "ed-login-view", {
+        /* LIFECYCLE */
+        ready: function() {
+          this.loginForm = this.shadowRoot.getElementById( "login-form" );
+          this.submitButton = this.shadowRoot.getElementById( "login-submit" );
+          this.formInputs = this.loginForm.shadowRoot.querySelectorAll( "ed-form-input" );
+        },
+        attached: function() {
+          this.submitButton.addEventListener( "click", this.submitForm.bind( this ) );
+        },
+        submitForm: function( event ) {
+          event.preventDefault();
+
+          var email = this.loginForm.querySelector( ".email" ).shadowRoot.querySelector( "input" ).value,
+            password = this.loginForm.querySelector( ".password" ).shadowRoot.querySelector( "input" ).value;
+
+          userService.login( email, password );
+        },
+        detached: function() {},
+        attributeChanged: function( attrName, oldValue, newValue ) {}
+        /* PROPERTIES */
+        /* METHODS */
+      });
+    })
+
+
+})( window.Polymer, window.System );
