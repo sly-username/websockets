a0316f623d308ae47c8bce0da05758a6288728b3
Khoa Ngo <khoa@eardish.com>
2015-04-16 12:53:46 -0700

    [#395] star rating completed

--- client/components/ed/ed-star-rating/ed-star-rating.js
+++ client/components/ed/ed-star-rating/ed-star-rating.js
@@ -4,12 +4,41 @@
   polymer( "ed-star-rating", {
     /* LIFECYCLE */
     disable: false,
-    ready: function() {},
-    attached: function() {},
-    detached: function() {},
-    attributeChanged: function( attrName, oldValue, newValue ) {}
+    ready: function() {
+      this.inputField = this.shadowRoot.getElementById( "input-field" );
+      this.overlapField = this.shadowRoot.getElementById( "overlap-field" );
+    },
+    attached: function() {
+      this.inputField.addEventListener( "click", this.triggerRating.bind( this ));
+    },
+    triggerRating: function( event ) {
+      switch ( event.target.id ) {
+        case "rate1":
+          this.transformOverlap( -80 );
+          break;
+        case "rate2":
+          this.transformOverlap( -60 );
+          break;
+        case "rate3":
+          this.transformOverlap( -40 );
+          break;
+        case "rate4":
+          this.transformOverlap( -20 );
+          break;
+        case "rate5":
+          this.transformOverlap( 0 );
+          break;
+        default:
+          // do nothing
+          break;
+      }
+    },
+    transformOverlap: function( percent ) {
+      this.overlapField.style.transform = "translateX(" + percent + "%)";
+      this.overlapField.style.webkitTransform = "translateX(" + percent + "%)";
+      this.overlapField.style.msTransform = "translateX(" + percent + "%)";
+    }
     /* PROPERTIES */
     /* METHODS */
   });
-
 })( window.Polymer );
