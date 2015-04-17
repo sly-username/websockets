a515ae8993ba6b7e6cf39545930d94bcffb31621
Khoa Ngo <khoa@eardish.com>
2015-04-16 18:26:08 -0700

    [#395] Cleaned up css and added in hover logic

--- client/components/ed/ed-star-rating/ed-star-rating.js
+++ client/components/ed/ed-star-rating/ed-star-rating.js
@@ -7,22 +7,39 @@
     ready: function() {
       this.inputField = this.shadowRoot.getElementById( "input-field" );
       this.overlapField = this.shadowRoot.getElementById( "overlap-field" );
+      this.secondInput = this.shadowRoot.getElementById( "rate2" );
+      this.thirdInput = this.shadowRoot.getElementById( "rate3" );
+      this.forthInput = this.shadowRoot.getElementById( "rate4" );
+      this.fifthInput = this.shadowRoot.getElementById( "rate5" );
     },
     attached: function() {
       this.inputField.addEventListener( "click", this.triggerRating.bind( this ));
+      this.inputField.addEventListener( "mouseover", this.triggerRating.bind( this ));
     },
     triggerRating: function( event ) {
       switch ( event.target.id ) {
         case "rate1":
+          if ( this.fifthInput.checked || this.forthInput.checked || this.thirdInput.checked || this.secondInput.checked ) {
+            break;
+          }
           this.transformOverlap( -80 );
           break;
         case "rate2":
+          if ( this.fifthInput.checked || this.forthInput.checked || this.thirdInput.checked ) {
+            break;
+          }
           this.transformOverlap( -60 );
           break;
         case "rate3":
+          if ( this.fifthInput.checked || this.forthInput.checked ) {
+            break;
+          }
           this.transformOverlap( -40 );
           break;
         case "rate4":
+          if ( this.fifthInput.checked ) {
+            break;
+          }
           this.transformOverlap( -20 );
           break;
         case "rate5":
@@ -38,7 +55,5 @@
       this.overlapField.style.webkitTransform = "translateX(" + percent + "%)";
       this.overlapField.style.msTransform = "translateX(" + percent + "%)";
     }
-    /* PROPERTIES */
-    /* METHODS */
   });
 })( window.Polymer );
