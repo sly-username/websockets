e5573d3905896e96b32cc41e3bf346132439af15
Khoa Ngo <khoa@eardish.com>
2015-04-17 13:01:15 -0700

    [#445] fixes for ed-mini-player, ed-paired-input and song-card-scrubber

--- client/components/ed/ed-mini-player/ed-mini-player.js
+++ client/components/ed/ed-mini-player/ed-mini-player.js
@@ -1,25 +1,30 @@
 ( function( polymer ) {
   "use strict";
 
+  var swapIconHandler = function() {
+    if ( this.playIcon.getAttribute( "name" ) === "play" ) {
+      this.playIcon.setAttribute( "name", "pause" );
+    } else {
+      this.playIcon.setAttribute( "name", "play" );
+    }
+  };
+
   polymer( "ed-mini-player", {
     /* LIFECYCLE */
     ready: function() {
       this.playBtn = this.shadowRoot.getElementById( "play-btn" );
       this.playIcon = this.shadowRoot.getElementById( "play-icon" );
+      this.handlers = {
+        swapIcon: swapIconHandler.bind( this )
+      };
     },
     attached: function() {
-      this.playBtn.addEventListener( "click", this.swapIcon.bind( this ) );
-      this.playBtn.addEventListener( "tap", this.swapIcon.bind( this ) );
+      this.playBtn.addEventListener( "click", this.handlers.swapIcon );
+      this.playBtn.addEventListener( "tap", this.handlers.swapIcon );
     },
-    attributeChanged: function( attrName, oldValue, newValue ) {},
-    swapIcon: function() {
-      if ( this.playIcon.getAttribute( "name" ) === "play" ) {
-        this.playIcon.setAttribute( "name", "pause" );
-      } else {
-        this.playIcon.setAttribute( "name", "play" );
-      }
+    detached: function() {
+      this.playBtn.removeEventListener( "click", this.handlers.swapIcon );
+      this.playBtn.removeEventListener( "tap", this.handlers.swapIcon );
     }
-    /* PROPERTIES */
-    /* METHODS */
   });
 })( window.Polymer );
