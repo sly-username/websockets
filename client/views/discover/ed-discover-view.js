f039042b968a7d9423e7b2e9499139a82d8d8b58
sly <sylvia@eardish.com>
2015-04-16 20:28:13 -0700

    [#437] Set up html and js files.
    Still figuring out how to structure html and then attack the css.

--- client/views/discover/ed-discover-view.js
+++ client/views/discover/ed-discover-view.js
@@ -1,3 +1,17 @@
-/**
- * Created by shwang on 4/16/15.
- */
+( function( polymer, System ) {
+  "use strict";
+
+  System.import( "domain/ed/services/ed-discover-service" )
+    .then(function( imported ) {
+      var discoverService = imported.default;
+
+      polymer( "ed-discover-view", {
+        ready: function() {
+        },
+        attached: function() {
+        },
+        detached: function() {},
+        attributeChanged: function( attrName, oldValue, newValue ) {}
+      });
+    });
+})( window.Polymer, window.System );
