3e2106c5abc305f05a1917ffa473d023e6eaa110
Khoa Ngo <kaythxs@gmail.com>
2015-04-20 22:17:38 -0700

    [#438] scaffolded out template view

---
+++ client/views/charts/ed-charts-view/ed-charts-view.js
@@ -0,0 +1,22 @@
+( function( polymer ) {
+  "use strict";
+
+  polymer( "ed-charts-view", {
+    /* LIFECYCLE */
+    // placeholder data, replace with data object
+    _options: [
+      {
+        rank: 1,
+        image: "http://fillmurray.com/200/300",
+        name: "Fill Murray",
+        points: 22732
+      }
+    ],
+    ready: function() {},
+    attached: function() {},
+    detached: function() {},
+    attributeChanged: function( attrName, oldValue, newValue ) {}
+    /* PROPERTIES */
+    /* METHODS */
+  });
+})( window.Polymer );
