182fa0f2fa42f6c6d88138403a35118358db7565
Khoa Ngo <kaythxs@gmail.com>
2015-04-21 22:02:08 -0700

    [#378] got button in settings component working, added minor animation, fixed wierd display issue

--- client/views/settings/ed-settings-view.js
+++ client/views/settings/ed-settings-view.js
@@ -1,14 +1,35 @@
 ( function( polymer ) {
   "use strict";
+  var triggerMenuHandler = function() {
+    if ( this.edMenu.hasAttribute( "hide" ) ) {
+      this.edMenu.removeAttribute( "hide" );
+      this.edTitle.style.opacity = 0;
+    } else {
+      this.edMenu.setAttribute( "hide", "" );
+      this.edTitle.style.opacity = 1;
+    }
+  }
 
   polymer( "ed-settings-view", {
     /* LIFECYCLE */
-    ready: function() {},
-    attached: function() {},
-    detached: function() {},
+    ready: function() {
+      this.edMenu = this.parentNode.parentNode.parentNode.getElementsByTagName( "ed-side-menu" )[0];
+      this.edTitle = this.shadowRoot.getElementById( "settings-title" );
+      this.triggerBtn = this.shadowRoot.getElementById( "menu-trigger" );
+      this.handlers = {
+        triggerMenu: triggerMenuHandler.bind( this )
+      };
+    },
+    attached: function() {
+      this.triggerBtn.addEventListener( "click", this.handlers.triggerMenu );
+      this.triggerBtn.addEventListener( "tap", this.handlers.triggerMenu );
+    },
+    detached: function() {
+      this.triggerBtn.removeEventListener( "click", this.handlers.triggerMenu );
+      this.triggerBtn.removeEventListener( "tap", this.handlers.triggerMenu );
+    },
     attributeChanged: function( attrName, oldValue, newValue ) {}
     /* PROPERTIES */
     /* METHODS */
   });
-
 })( window.Polymer );
