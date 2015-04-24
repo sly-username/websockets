85b4a90b31b440fe19bc45a4d6503fadfd070d13
Khoa Ngo <kaythxs@gmail.com>
2015-04-24 15:43:30 -0700

    [#484] added logic for adding to array

--- client/views/onboarding/ed-liked-view/ed-liked-view.js
+++ client/views/onboarding/ed-liked-view/ed-liked-view.js
@@ -1,13 +1,41 @@
 ( function( polymer ) {
   "use strict";
 
-  polymer( "ed-liked-view", {
-    /* LIFECYCLE */
-    ready: function() {},
-    attached: function() {},
-    detached: function() {},
-    attributeChanged: function( attrName, oldValue, newValue ) {}
-    /* PROPERTIES */
-    /* METHODS */
+  Promise.all([
+    System.import( "domain/ed/services/ed-discover-service" )
+  ]).then(function( imported ) {
+    var
+      discoverService = imported[ 0 ].default,
+      bubbleCounter = 0,
+      bubbleArray = [],
+      i,
+      triggerBubblesHandler = function( event ) {
+        for ( i = 0; i < this.inputBubbles.length; i++ ) {
+          if ( this.inputBubbles[i].hasAttribute( "checked" ) ) {
+            bubbleArray.push( this.inputBubbles[i].getAttribute( "data-id" ) );
+          }
+        }
+        console.log( bubbleArray );
+      };
+
+    polymer( "ed-liked-view", {
+      /* LIFECYCLE */
+      ready: function() {
+        this.handlers = {
+          triggerBubbles: triggerBubblesHandler.bind( this )
+        };
+        this.inputBubbles = this.shadowRoot.querySelectorAll( "ed-bubble-select" );
+        this.nextBtn = this.shadowRoot.getElementById( "next-button" );
+      },
+      attached: function() {
+        this.nextBtn.addEventListener( "click", this.handlers.triggerBubbles );
+      },
+      detached: function() {
+        this.nextBtn.removeEventListener( "click", this.handlers.triggerBubbles );
+      },
+      attributeChanged: function( attrName, oldValue, newValue ) {}
+      /* PROPERTIES */
+      /* METHODS */
+    });
   });
 })( window.Polymer );
