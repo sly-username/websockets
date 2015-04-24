b948c112207378ac4620bc105e4c57a774493a41
sly <sylvia@eardish.com>
2015-04-23 20:29:41 -0700

    [#487] Attempting to add enter key functionality.
    There is a this issue.

--- client/views/referral/ed-referral-view.js
+++ client/views/referral/ed-referral-view.js
@@ -4,7 +4,6 @@
   System.import( "domain/ed/services/ed-user-service" )
     .then( function( imported ) {
       var userService = imported.default,
-        clickEvents = [ "mousedown", "touchstart" ],
         validateEmail = function() {
           var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
           if ( this.emailInput.validity.valid && re.test( this.emailInput.value )) {
@@ -19,16 +18,27 @@
         userService: userService,
         ready: function() {
           this.emailInput = this.shadowRoot.querySelector( "ed-form-input" ).shadowRoot.querySelector( "input" );
-          this.submitButton = this.shadowRoot.getElementById( "referral-submit" );
           this.emailInput.setAttribute( "autofocus", "" );
+          this.submitButton = this.shadowRoot.getElementById( "referral-submit" );
+          this.clickEvents = [ "mousedown", "touchstart" ];
+          this.triggerList = [ this.emailInput, this.submitButton ];
         },
         attached: function() {
-          clickEvents.forEach( function( eventName ) {
+          this.clickEvents.forEach( function( eventName ) {
             this.submitButton.addEventListener( eventName, this.submitFriendEmail.bind( this ));
           }.bind( this ));
-          // todo add return "keydown" event.keyCode === 13
 
           this.emailInput.addEventListener( "keyup", validateEmail.bind( this ));
+
+          this.triggerList.forEach( function( trigger ) {
+            trigger.addEventListener( "keydown", function( event ) {
+              if ( event.keyCode === 13 ) {
+                this.submitFriendEmail( event ).bind( this );
+              }
+
+              return false;
+            }.bind( this ));
+          });
         },
         detached: function() {},
         submitFriendEmail: function( event ) {
