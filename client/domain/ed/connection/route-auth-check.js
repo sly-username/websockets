b7bb35a7952005bf09fff3d0c37c562cc5b6cc99
Ryan Bogle <ryan@eardish.com>
2015-04-23 21:42:15 -0700

    Updated routes map in route checker.

--- client/domain/ed/connection/route-auth-check.js
+++ client/domain/ed/connection/route-auth-check.js
@@ -1,8 +1,48 @@
 /*eslint consistent-this: 0 */
 
-// add routes to this map with a boolean flag for needs auth true/false
+// add routes to this map with a boolean flag for:
+//  true --> needs auth
+//  false --> "public" route
 var routeMap = {
-  "profile/get": false
+  // User Routes
+  "user/create": false,
+  "user/password/get": false,
+  "user/password/set": false,
+  "user/password/update": true,
+
+  // Referral
+  "referral/get": true,
+  "referral/create": true,
+
+  // Track
+  "track/create": true,
+  "track/delete": true,
+  "track/get": true,
+  "track/url/get": true,
+  "track/list": true,
+  "track/rate/set": true,
+  "track/art/get": true,
+
+  // Discover
+  "discover/blend/get": true,
+  "discover/list": true,
+
+  // Profile
+  "profile/get": false,
+  "profile/update": true,
+  "profile/set": true,
+  "profile/create": true,
+  "profile/art/create": true,
+  "profile/art/delete": true,
+  "profile/art/update": true,
+  "profile/blend/get": true,
+  "profile/blend/set": true,
+
+  // Genre
+  "genre/list": true,
+
+  // Chart
+  "chart/list": true
 };
 
 export default {
