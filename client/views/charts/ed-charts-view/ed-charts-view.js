ab37c83b3e6c699db586aad3f6d238f36cc96140
Khoa Ngo <kaythxs@gmail.com>
2015-04-25 16:55:21 -0700

    [#378] added wrapper to charts view and charts now correctly pushes to the side

--- client/views/charts/ed-charts-view/ed-charts-view.js
+++ client/views/charts/ed-charts-view/ed-charts-view.js
@@ -1,5 +1,14 @@
 ( function( polymer ) {
   "use strict";
+  var triggerMenuHandler = function() {
+    if ( !this.edMenu.hasAttribute( "class" ) ) {
+      this.edMenu.setAttribute( "class", "show-menu" );
+      this.chartsTitle.style.opacity = 0;
+    } else {
+      this.edMenu.removeAttribute( "class" );
+      this.chartsTitle.style.opacity = 1;
+    }
+  }
 
   polymer( "ed-charts-view", {
     /* LIFECYCLE */
@@ -42,9 +51,22 @@
         points: 732
       }
     ],
-    ready: function() {},
-    attached: function() {},
-    detached: function() {},
+    ready: function() {
+      this.edMenu = document.getElementById( "side-menu" );
+      this.triggerBtn = this.shadowRoot.getElementById( "menu-trigger" );
+      this.chartsTitle = this.shadowRoot.getElementById( "charts-title" );
+      this.handlers = {
+        triggerMenu: triggerMenuHandler.bind( this )
+      };
+    },
+    attached: function() {
+      this.triggerBtn.addEventListener( "click", this.handlers.triggerMenu );
+      this.triggerBtn.addEventListener( "tap", this.handlers.triggerMenu );
+    },
+    detached: function() {
+      this.triggerBtn.removeEventListener( "click", this.handlers.triggerMenu );
+      this.triggerBtn.removeEventListener( "tap", this.handlers.triggerMenu );
+    },
     attributeChanged: function( attrName, oldValue, newValue ) {}
     /* PROPERTIES */
     /* METHODS */
