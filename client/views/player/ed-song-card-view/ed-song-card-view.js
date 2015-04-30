c6a5fb47cd968287c42e7f6a03c49765d69f14d3
randy rollog <rrollog@eardish.com>
2015-04-30 15:18:28 -0700

    [#377] moved profileblend discover call into the onboarding view

--- client/views/player/ed-song-card-view/ed-song-card-view.js
+++ client/views/player/ed-song-card-view/ed-song-card-view.js
@@ -92,8 +92,6 @@
 
         this.$[ "minify-icon" ].addEventListener( "click", this.handler.togglePlayer );
         this.$[ "mini-player-wrapper" ].addEventListener( "click", this.handler.togglePlayer );
-
-        playerService.startMusicDiscovery( "profileBlend" );
       },
       detached: function() {
         clearInterval( this.intervalId );
