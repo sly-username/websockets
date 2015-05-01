62901957a56b056f8e316290d56295ba0f2265f3
Khoa Ngo <kaythxs@gmail.com>
2015-04-30 18:56:59 -0700

    [#546] integrated with request view

--- client/views/onboarding/ed-forgot-pass-request-view/ed-forgot-pass-request-view.js
+++ client/views/onboarding/ed-forgot-pass-request-view/ed-forgot-pass-request-view.js
@@ -1,29 +1,48 @@
 ( function( polymer ) {
   "use strict";
-  var triggerSubmitHandler = function() {
-
-  };
 
   Promise.all([
-    System.import( "domain/ed/services/ed-discover-service" )
+    System.import( "domain/ed/services/ed-user-service" )
   ]).then(function( imported ) {
+    var
+      userService = imported.default,
+      emailCheckHandler = function() {
+      if ( this.emailInput.validity.valid ) {
+        this.submitBtn.removeAttribute( "disabled" );
+      } else {
+        this.submitBtn.setAttribute( "disabled", "" );
+      }
+    },
+      triggerSubmitHandler = function( event ) {
+        event.preventDefault();
+
+        return userService.forgotPassword( this.emailInput.value )
+          .then(function( response ) {
+            console.log( response );
+            return response;
+          }.bind( this ));
+      };
+
     polymer( "ed-forgot-pass-request-view", {
       /* LIFECYCLE */
       ready: function() {
         this.submitBtn = this.shadowRoot.getElementById( "forgot-submit" );
+        this.emailInput = this.shadowRoot.querySelector( ".email" )
+          .shadowRoot.querySelector( "input" );
+        console.log( this.emailInput );
         this.handlers = {
-          triggerSubmit: triggerSubmitHandler.bind( this )
+          triggerSubmit: triggerSubmitHandler.bind( this ),
+          emailCheck: emailCheckHandler.bind( this )
         };
       },
       attached: function() {
         this.submitBtn.addEventListener( "click", this.handlers.triggerSubmit );
+        this.emailInput.addEventListener( "keyup", this.handlers.emailCheck );
       },
       detached: function() {
         this.submitBtn.removeEventListener( "click", this.handlers.triggerSubmit );
-      },
-      attributeChanged: function( attrName, oldValue, newValue ) {}
-      /* PROPERTIES */
-      /* METHODS */
+        this.emailInput.addEventListener( "keyup", this.handlers.emailCheck );
+      }
     });
   });
 })( window.Polymer );
