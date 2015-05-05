78c025e72566aee37f22451e46433b8a695059a0
randy rollog <rrollog@eardish.com>
2015-05-05 15:40:59 -0700

    [#544] updated socket url

--- client/domain/ed/urls/dev.js
+++ client/domain/ed/urls/dev.js
@@ -1,7 +1,6 @@
 export default {
   get path() {
-    return "ws://apiqa.eardish.com";
-    //return "ws://apidev.eardishcorp.com:80";
+    return "ws://apidev.eardishcorp.com:80";
     //return "ws://echo.websocket.org";
   }
 }
