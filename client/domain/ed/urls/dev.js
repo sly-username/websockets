d2d7f1b2e233660f1f1b04f5aa3950503f6b793f
randy rollog <rrollog@eardish.com>
2015-05-06 00:10:51 -0700

    [#544] removed login and changed socket url

--- client/domain/ed/urls/dev.js
+++ client/domain/ed/urls/dev.js
@@ -1,7 +1,6 @@
 export default {
   get path() {
-    return "ws://10.151.1.152:80";
-    //return "ws://apidev.eardishcorp.com:80";
+    return "ws://apidev.eardishcorp.com:80";
     //return "ws://echo.websocket.org";
   }
 }
