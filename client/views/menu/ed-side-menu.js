a4fbf13e4fc881d6cf83b5c4d5e34a43e26c19bb
Khoa Ngo <khoa@eardish.com>
2015-05-06 14:15:16 -0700

    [#524] Menu now closes on route click

--- client/views/menu/ed-side-menu.js
+++ client/views/menu/ed-side-menu.js
@@ -3,7 +3,13 @@
 
   System.import( "domain/ed/services/ed-data-service" )
     .then(function( imported ) {
-      var dataService = imported.default;
+      var
+        dataService = imported.default,
+        triggerMenuHandler = function( event ) {
+          if ( event.target.hasAttribute( "href" ) ) {
+            this.edMenu.classList.toggle( "show-menu" );
+          }
+        };
 
       polymer( "ed-side-menu", {
         /* LIFECYCLE */
@@ -16,9 +22,20 @@
                 console.dir( this );
               }.bind( this ));
           }
+          this.menuList = this.shadowRoot.getElementById( "menu-list" );
+          this.edMenu = document.getElementById( "side-menu" );
+
+          // handlers
+          this.handlers = {
+            triggerMenu: triggerMenuHandler.bind( this )
+          };
         },
-        attached: function() {},
-        detached: function() {},
+        attached: function() {
+          this.menuList.addEventListener( "click", this.handlers.triggerMenu );
+        },
+        detached: function() {
+          this.menuList.removeEventListener( "click", this.handlers.triggerMenu );
+        },
         "ed-idChanged": function() {
           this.attributeChanged( "ed-id" );
         },
