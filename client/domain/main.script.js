9e0fa6d5957ccdd1d13e14d3d0f1d9cf103d9bbb
Ryan Bogle <ryan@eardish.com>
2015-05-08 14:22:33 -0700

    [#587] Little bit of cleanup
    Added back a "root" route that goes to the login page.
    Added a little more logic to the player hide/show logic.
    Fixed circular reference error with the route change analytic event.

--- client/domain/main.script.js
+++ client/domain/main.script.js
@@ -1,6 +1,19 @@
 (function( window, Promise, indexedDB ) {
   "use strict";
 
+  var
+    hidePlayerRoutes = [
+      "/",
+      "/login",
+      "/register",
+      "/registration"
+    ],
+    needToHidePlayerForRoute = function( route ) {
+      return hidePlayerRoutes.some( hideRoute => {
+        return hideRoute === route;
+      });
+    };
+
   // TODO REMOVE DEBUG
   // REMOVE DATABASES
   indexedDB.deleteDatabase( "profile" );
@@ -9,17 +22,24 @@
   Promise.all([
     System.import( "domain/ed/services/ed-data-service" ),
     System.import( "domain/ed/services/ed-connection-service" ),
-    System.import( "domain/ed/services/ed-user-service" )
+    System.import( "domain/ed/services/ed-user-service" ),
+    System.import( "domain/ed/services/ed-player-service" )
   ]).then( imports => {
-    // TODO make polymer component?
     var
+      [
+        dataService,
+        connectionService,
+        userService,
+        playerService
+      ] = imports.map( imported => imported.default ),
       animationWrapper = document.getElementById( "animation-wrapper" ),
       songCard = document.getElementById( "song-card" ),
       router = document.querySelector( "app-router" );
 
     router.addEventListener( "state-change", function( event ) {
-      // TODO add additional routes
-      if ( event.detail.path === "/" ) {
+      console.log( "in state-change event: %o", event.detail );
+
+      if ( !playerService.isPlaying || needToHidePlayerForRoute( event.detail.path ) ) {
         songCard.classList.add( "hidden" );
         animationWrapper.classList.remove( "player-padding" );
       } else {
@@ -31,13 +51,11 @@
       }
     });
 
-    //need to init manually to ensure event binding
+    // need to init manually to ensure event binding
     router.init();
-
-    console.log( "PromisedDB/edDataService Loaded %o", imports );
   })
   .catch( error => {
-    console.error( error.message );
+    console.error( "Problem in main script" );
     console.error( error.stack );
   });
 })( window, window.Promise, window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB );
