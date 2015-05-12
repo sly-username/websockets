723a475e9b73dff15c9ac46ad6172c2263f08bee
Khoa Ngo <khoa@eardish.com>
2015-05-11 18:18:21 -0700

    [#583] registration is fully formed

--- client/views/onboarding/ed-disliked-view/ed-disliked-view.js
+++ client/views/onboarding/ed-disliked-view/ed-disliked-view.js
@@ -2,12 +2,10 @@
   "use strict";
 
   Promise.all([
-    System.import( "domain/ed/services/ed-discover-service" ),
-    System.import( "domain/ed/services/ed-player-service" )
+    System.import( "domain/ed/services/ed-discover-service" )
   ]).then(function( imported ) {
     var
       discoverService = imported[ 0 ].default,
-      playerService = imported[ 1 ].default,
       bubbleCounter = 0,
       bubbleArray = [],
       triggerBubblesHandler = function( event ) {
@@ -24,9 +22,7 @@
         console.log( "trigger bubbles handler %o, %o", this.likedBubbles, bubbleArray );
         return discoverService.setCurrentProfileBlend( this.likedBubbles, bubbleArray )
           .then(function( response ) {
-            // todo go to song card and start playing discover blend
-            playerService.startMusicDiscovery( "profileBlend" );
-
+            this.router.go( "/discover" );
             return response;
           }.bind( this ));
       },
