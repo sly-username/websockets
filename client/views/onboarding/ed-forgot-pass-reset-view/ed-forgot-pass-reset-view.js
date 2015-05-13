99a85e6e8a3ff20181a6f23839fc158bf03eb920
sly <sylvia@eardish.com>
2015-05-12 17:56:35 -0700

    [#593] Forget password reset view has the client side validations before sending.

--- client/views/onboarding/ed-forgot-pass-reset-view/ed-forgot-pass-reset-view.js
+++ client/views/onboarding/ed-forgot-pass-reset-view/ed-forgot-pass-reset-view.js
@@ -18,6 +18,8 @@
           .shadowRoot.querySelector( ".ed-paired-input-primary-wrapper" );
         this.confirmBox = this.shadowRoot.querySelector( ".password" )
           .shadowRoot.querySelector( ".ed-paired-input-confirm-wrapper" );
+        this.confirmInput = this.shadowRoot.querySelector( ".password" )
+          .shadowRoot.querySelector( "input#confirm-box" );
 
         // todo change to .value
         this.resetCodeInput = this.shadowRoot.querySelector( ".reset-code" )
@@ -25,25 +27,20 @@
         this.resetCodeBox = this.shadowRoot.querySelector( ".reset-code" )
           .shadowRoot.querySelector( ".form-input-container" );
 
-
         this.resetCodeError = this.shadowRoot.querySelector( ".reset-code-error" );
         this.matchError = this.shadowRoot.querySelector( ".match-error" );
         this.patternError = this.shadowRoot.querySelector( ".pattern-error" );
       },
       attached: function() {
         clickEvents.forEach( function( eventName ) {
-          this.submitButton.addEventListener( eventName, this.validatePasswordPair.bind( this ) );
-          this.submitButton.addEventListener( eventName, this.validateRegexPattern.bind( this ) );
-          this.submitButton.addEventListener( eventName, this.validateCodeInput.bind( this ) );
+          this.submitButton.addEventListener( eventName, this.submitCheck.bind( this ) );
         }.bind( this ));
 
         this.submitButton.addEventListener( "keypress", this.validateAfterEnter.bind( this ) );
       },
       detached: function() {
         clickEvents.forEach( function( eventName ) {
-          this.submitButton.removeEventListener( eventName, this.validatePasswordPair.bind( this ) );
-          this.submitButton.removeEventListener( eventName, this.validateRegexPattern.bind( this ) );
-          this.submitButton.removeEventListener( eventName, this.validateCodeInput.bind( this ) );
+          this.submitButton.removeEventListener( eventName, this.submitCheck.bind( this ) );
         }.bind( this ));
 
         this.submitButton.removeEventListener( "keypress", this.validateAfterEnter.bind( this ) );
@@ -51,28 +48,34 @@
       validateCodeInput: function() {
         if ( this.resetCodeInput.value !== "" ) {
           this.resetCodeBox.classList.remove( "invalid-field" );
+          return true;
         } else if ( this.resetCodeInput.value === "" ) {
           this.resetCodeBox.classList.add( "invalid-field" );
           this.resetCodeError.innerHTML = "Wrong. Please check your reset code and try again."
+          return false;
         }
       },
       validatePasswordPair: function() {
         if ( this.pairedInput.inputMatchConfirm ) {
           this.primaryBox.classList.remove( "invalid-field" );
           this.confirmBox.classList.remove( "invalid-field" );
+          return true;
         } else if ( !this.pairedInput.inputMatchConfirm ) {
           this.primaryBox.classList.add( "invalid-field" );
           this.confirmBox.classList.add( "invalid-field" );
           this.matchError.innerHTML = "Wrong. Passwords Do Not Match.";
+          return false;
         }
       },
       validateRegexPattern: function() {
         if ( this.pairedInput.regexConfirm ) {
           console.log( this.primaryBox );
           this.primaryBox.classList.remove( "invalid-field" );
+          return true;
         } else if ( !this.pairedInput.regexConfirm ) {
           this.primaryBox.classList.add( "invalid-field" );
           this.patternError.innerHTML = "Wrong. Password Must Be At Least 8 Characters.";
+          return false;
         }
       },
       validateAfterEnter: function( event ) {
@@ -84,14 +87,21 @@
           this.validateCodeInput();
         }
       },
+      submitCheck: function() {
+        var validateCode = this.validateCodeInput(),
+        validatePassword = this.validatePasswordPair(),
+        validateRegex = this.validateRegexPattern();
+        if ( validateCode && validatePassword && validateRegex ) {
+          console.log( "test" );
+          this.submitResetPassword();
+        }
+      },
       submitResetPassword: function() {
-        // if validatePasswordPair && validateRegexPattern
-        // then submit form
-        // userService.resetPassword( this.resetCodeInput.value, this.confirmInput.value )
-        // .then( function( response ) {
-        //  console.log( response );
-        //  return response;
-        //} .bind( this ));
+        return userService.resetPassword( this.resetCodeInput.value, this.confirmInput.value )
+          .then ( function ( response ) {
+            console.log ( response );
+            return response;
+          });
       }
     });
   });
