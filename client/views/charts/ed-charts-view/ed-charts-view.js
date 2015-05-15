d35a1dbace71082e3df268bcbe5b0076dbad5674
sly <sylvia@eardish.com>
2015-05-15 16:14:07 -0700

    [#618] Changing logic for carousel animation.
    Committing here because big changes are ahead. Possibly breaking changes ...

--- client/views/charts/ed-charts-view/ed-charts-view.js
+++ client/views/charts/ed-charts-view/ed-charts-view.js
@@ -7,64 +7,68 @@
     var discoverService = imported[ 0 ].default,
       currentView = 1,
       chartNames = [ "most-tracks-rated-fan", "completed-listens-fan", "completed-listens-track", "highest-rated-track" ],
-      updateChartClass = function( classesRemoveArray, classAdd, self ) {
+      updateChartPosition = function( classesRemoveArray, classAdd, self ) {
         classesRemoveArray.forEach( function( classNumber ) {
           self.singleChartWrapper.classList.remove( classNumber );
           self.singleChartWrapper.classList.add( classAdd );
         });
       },
       updateChartsViewHandler = function( event ) {
-        var eventType = event.detail.name;
 
-        if ( eventType === "moveLeft" ) {
-          switch ( currentView ) {
-            case 1:
-              updateChartClass( [ "one", "three", "four" ], "two", this );
-              currentView = 2;
-              break;
-            case 2:
-              updateChartClass( [ "one", "two", "four" ], "three", this );
-              currentView = 3;
-              break;
-            case 3:
-              updateChartClass( [ "one", "two", "four" ], "four", this );
-              currentView = 4;
-              break;
-            case 4:
-              updateChartClass( [ "two", "three", "four" ], "one", this );
-              // todo disable final view from going left
-              currentView = 1;
-              break;
-            default:
-              // fall through
-              break;
-          }
-        }
 
-        if ( eventType === "moveRight" ) {
-          switch ( currentView ) {
-            case 1:
-              // todo disable first view from going right
-              updateChartClass( [ "one", "three", "four" ], "two", this );
-              currentView = 2;
-              break;
-            case 2:
-              updateChartClass( [ "one", "two", "four" ], "three", this );
-              currentView = 3;
-              break;
-            case 3:
-              updateChartClass( [ "one", "two", "four" ], "four", this );
-              currentView = 4;
-              break;
-            case 4:
-              updateChartClass( [ "two", "three", "four" ], "one", this );
-              currentView = 1;
-              break;
-            default:
-              // fall through
-              break;
-          }
-        }
+
+
+        //var eventType = event.detail.name;
+        //
+        //if ( eventType === "moveLeft" ) {
+        //  switch ( currentView ) {
+        //    case 1:
+        //      updateChartClass( [ "one", "three", "four" ], "two", this );
+        //      currentView = 2;
+        //      break;
+        //    case 2:
+        //      updateChartClass( [ "one", "two", "four" ], "three", this );
+        //      currentView = 3;
+        //      break;
+        //    case 3:
+        //      updateChartClass( [ "one", "two", "four" ], "four", this );
+        //      currentView = 4;
+        //      break;
+        //    case 4:
+        //      updateChartClass( [ "two", "three", "four" ], "one", this );
+        //      // todo disable final view from going left
+        //      currentView = 1;
+        //      break;
+        //    default:
+        //      // fall through
+        //      break;
+        //  }
+        //}
+        //
+        //if ( eventType === "moveRight" ) {
+        //  switch ( currentView ) {
+        //    case 1:
+        //      // todo disable first view from going right
+        //      updateChartClass( [ "one", "three", "four" ], "two", this );
+        //      currentView = 2;
+        //      break;
+        //    case 2:
+        //      updateChartClass( [ "one", "two", "four" ], "three", this );
+        //      currentView = 3;
+        //      break;
+        //    case 3:
+        //      updateChartClass( [ "one", "two", "four" ], "four", this );
+        //      currentView = 4;
+        //      break;
+        //    case 4:
+        //      updateChartClass( [ "two", "three", "four" ], "one", this );
+        //      currentView = 1;
+        //      break;
+        //    default:
+        //      // fall through
+        //      break;
+        //  }
+        //}
       };
 
     polymer( "ed-charts-view", {
