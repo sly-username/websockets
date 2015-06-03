cde90e164c3692a3c201f214cc816be886bd72e4
randy rollog <rrollog@eardish.com>
2015-06-03 16:47:48 -0700

    [#769] hiding banner on certain routes

--- client/domain/ed/cordova/ed-admob.js
+++ client/domain/ed/cordova/ed-admob.js
@@ -3,28 +3,11 @@ var
   AdMob = window.AdMob,
   admobid = {};
 
-if ( /(android)/i.test( navigator.userAgent ) ) {
-  admobid = { // for Android
-    banner: 'ca-app-pub-6869992474017983/9375997553',
-    interstitial: 'ca-app-pub-6869992474017983/1657046752'
-  };
-} else if ( /(ipod|iphone|ipad)/i.test( navigator.userAgent ) ) {
-  admobid = { // for iOS
-    banner: 'ca-app-pub-6869992474017983/4806197152',
-    interstitial: 'ca-app-pub-6869992474017983/7563979554'
-  };
-} else {
-  admobid = { // for Windows Phone
-    banner: 'ca-app-pub-6869992474017983/8878394753',
-    interstitial: 'ca-app-pub-6869992474017983/1355127956'
-  };
-}
-
 if ( AdMob ) {
   AdMob.createBanner({
-    adId: admobid.banner,
+    adId: "ca-app-pub-6485453766683902/8331842279",
     position: AdMob.AD_POSITION.TOP_CENTER,
-    autoShow: true
+    autoShow: false
   });
 } else {
   console.warn( "AdMob module ran, but AdMob object not ready" );
