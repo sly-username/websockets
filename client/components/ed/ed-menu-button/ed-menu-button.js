e8c6b2842bff067da19566a9bc13e395b3b1e2fc
Ryan Bogle <ryan.bogle@gmail.com>
2015-06-04 10:27:50 -0700

    Menu button now uses on-tap

--- client/components/ed/ed-menu-button/ed-menu-button.js
+++ client/components/ed/ed-menu-button/ed-menu-button.js
@@ -1,25 +1,17 @@
 ( function( polymer ) {
   "use strict";
-  var triggerMenuHandler = function() {
-    this.edMenu.classList.toggle( "show-menu" );
-  };
 
   polymer( "ed-menu-button", {
     /* LIFECYCLE */
     ready: function() {
       this.edMenu = document.getElementById( "side-menu" );
-      this.triggerBtn = this.shadowRoot.getElementById( "menu-trigger" );
-      this.handlers = {
-        triggerMenu: triggerMenuHandler.bind( this )
-      };
     },
-    attached: function() {
-      this.triggerBtn.addEventListener( "mouseup", this.handlers.triggerMenu );
-    },
-    detached: function() {
-      this.triggerBtn.removeEventListener( "mouseup", this.handlers.triggerMenu );
-    },
-    attributeChanged: function( attrName, oldValue, newValue ) {}
+    // attached: function() {},
+    // detached: function() {},
+    // attributeChanged: function( attrName, oldValue, newValue ) {}
+    triggerMenu: function() {
+      this.edMenu.classList.toggle( "show-menu" );
+    }
     /* PROPERTIES */
     /* METHODS */
   });
