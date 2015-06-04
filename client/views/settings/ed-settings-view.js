d1fb3c0857ca0b26fed1cf80c6c0b324289cb767
Ryan Bogle <ryan@eardish.com>
2015-06-04 09:24:21 -0700

    BONUS! Settings page now uses on-tap events!

--- client/views/settings/ed-settings-view.js
+++ client/views/settings/ed-settings-view.js
@@ -1,54 +1,45 @@
-( function( polymer ) {
+(function( polymer ) {
   "use strict";
   Promise.all([
     System.import( "domain/ed/services/ed-user-service" )
   ]).then(function( imported ) {
     var
-      userService = imported[ 0 ].default,
-      logOutTriggerHandler = function() {
+      userService = imported[ 0 ].default;
+
+    polymer( "ed-settings-view", {
+      // ready: function() {},
+      // attached: function() {},
+      // detached: function() {},
+      redirectInAppHandler: function( event ) {
+        var
+          url,
+          cordova = window.cordova;
+
+        switch ( event.path[ 0 ].id ) {
+          case "Terms-button":
+            url = "http://www.eardish.com/terms";
+            break;
+          case "About-button":
+            url = "http://www.eardish.com/privacy";
+            break;
+          case "Support-button":
+            url = "http://www.eardish.com/support";
+            break;
+          default:
+            // do nothing
+            return;
+        }
+
+        if ( cordova && cordova.InAppBrowser && typeof cordova.InAppBrowser.open === "function" ) {
+          cordova.InAppBrowser.open( url, "_system" );
+        } else {
+          window.open( url, "_blank" );
+        }
+      },
+      logOutTriggerHandler: function() {
         userService.logout();
         this.router.go( "/login" );
         return false;
-      },
-      redirectInAppHandler = function( event ) {
-        if ( cordova && cordova.InAppBrowser && typeof cordova.InAppBrowser.open === "function" ) {
-          switch ( event.target.id ) {
-            case "Terms-button":
-              cordova.InAppBrowser.open( "http://www.eardish.com/terms", "_blank" );
-              break;
-            case "About-button":
-              cordova.InAppBrowser.open( "http://www.eardish.com/privacy", "_blank" );
-              break;
-            case "Support-button":
-              cordova.InAppBrowser.open( "http://www.eardish.com/support", "_blank" );
-              break;
-            default:
-              // do nothing
-              break;
-          }
-        }
-      };
-
-    polymer( "ed-settings-view", {
-      ready: function() {
-        this.logOut = this.shadowRoot.getElementById( "log-out-button" );
-        this.termsBtn = this.shadowRoot.getElementById( "Terms-button" );
-        this.aboutBtn = this.shadowRoot.getElementById( "About-button" );
-
-        this.handlers = {
-          logOutTrigger: logOutTriggerHandler.bind( this ),
-          redirectInApp: redirectInAppHandler.bind( this )
-        };
-      },
-      attached: function() {
-        this.logOut.addEventListener( "touchstart", this.handlers.logOutTrigger );
-        this.termsBtn.addEventListener( "touchstart", this.handlers.redirectInApp );
-        this.aboutBtn.addEventListener( "touchstart", this.handlers.redirectInApp );
-      },
-      detached: function() {
-        this.logOut.removeEventListener( "touchstart", this.handlers.logOutTrigger );
-        this.termsBtn.removeEventListener( "touchstart", this.handlers.redirectInApp );
-        this.aboutBtn.removeEventListener( "touchstart", this.handlers.redirectInApp );
       }
     });
   });
