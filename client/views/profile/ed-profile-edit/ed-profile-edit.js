ff39780185f904433b7a9ec6b69d58674c8342e4
randy rollog <rrollog@eardish.com>
2015-06-09 17:48:32 -0700

    [#764] added cordova event backbutton

--- client/views/profile/ed-profile-edit/ed-profile-edit.js
+++ client/views/profile/ed-profile-edit/ed-profile-edit.js
@@ -27,8 +27,6 @@
         },
         uploadPhotoHandler = function( event ) {
           userService.changeProfileImage( event.target.files[ 0 ]);
-        },
-        backButtonHandler = function( event ) {
         };
 
       polymer( "ed-profile-edit", {
@@ -36,11 +34,9 @@
         ready: function() {
           this.nameInputsWrapper = this.shadowRoot.querySelector( ".name-field" );
           this.choosePhoto = this.shadowRoot.getElementById( "choose-photo" );
-          this.goBackButton = this.shadowRoot.getElementById( "edit-back" );
 
           this.handler = {
-            nameInput: nameInputHandler.bind( this ),
-            backButton: backButtonHandler.bind( this )
+            nameInput: nameInputHandler.bind( this )
           };
         },
         attached: function() {
@@ -54,12 +50,10 @@
 
           this.nameInputsWrapper.addEventListener( "blur", this.handler.nameInput, true );
           this.choosePhoto.addEventListener( "change", uploadPhotoHandler );
-          this.goBackButton.addEventListener( "backbutton", this.handler.backButton );
         },
         detached: function() {
           this.nameInputsWrapper.removeEventListener( "blur", this.handler.nameInput );
           this.choosePhoto.removeEventListener( "change", uploadPhotoHandler );
-          this.goBackButton.removeEventListener( "backbutton", this.handler.backButton );
         },
         // attributeChanged: function( attrName ) {}
         takePhoto: function() {
@@ -80,6 +74,9 @@
               });
             });
           }
+        },
+        goBack: function() {
+          navigator.app.backHistory();
         }
       });
     });
